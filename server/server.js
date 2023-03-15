08463420f1a07e1fe1d0b3fe6ad6efbe2a3127b6
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-15 12:34:44 -0400

    set up backend server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,46 @@
-// entry point for express (app.use)
+const express = require('express');
+const app = express();
+const path = require('path');
+// const dotenv = require('dotenv').config();
+const volleyball = require('volleyball');
+// const config = require('config');
+const PORT = process.env.PORT_NUMBER || 8080;
 
-// static middleware
+// Static middleware
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
-// logging middleware
+// logging middleware - disable during test
+// if (config.util.getEnv('NODE_ENV') !== 'test') {
+//   app.use(volleyball);
+// }
 
-// body parsing middleware
+// Body parsing middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// start of API routes
+// Start of API routes
+// app.use('/api', require('./API'));
 
-// serve html file
+app.get('/ping', function (req, res) {
+  return res.json('pong');
+});
 
-// 500 default error handling
+// Serves HTML file
+app.use('*', (req, res, next) => {
+  res.status(200).sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// Default error handling
+app.use((err, req, res, next) => {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error');
+});
+
+async function init() {
+  app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`);
+  });
+}
+
+init();
