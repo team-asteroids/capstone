a185856a4687b76145219415023d95abec7863b1
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-16 17:41:02 -0400

    add seed script and update seed file

--- seed.js
+++ seed.js
@@ -1,30 +1,32 @@
 // import mock data and models to seed db
 const {
   db,
-  User,
-  Sitter,
-  Sitter_Review,
-  Sitter_Rating,
-  Sitter_Pref,
-  Sitter_Client,
-  Pet,
-  Pet_Detail,
-  Booking,
-  Event,
-  Group,
-  Group_Post,
-  Message,
-  Map,
-  Post,
-  Post_Comment,
-  Access,
+  // User,
+  // Sitter,
+  // Sitter_Review,
+  // Sitter_Rating,
+  // Sitter_Pref,
+  // Sitter_Client,
+  // Pet,
+  // Pet_Detail,
+  // Booking,
+  // Event,
+  // Group,
+  // Group_Post,
+  // Message,
+  // Map,
+  // Post,
+  // Post_Comment,
+  // Access,
 } = require('./server/db/index');
 
 const init = async () => {
   try {
     db.sync({ force: true });
+    console.log('syncing the db');
   } catch (err) {
     console.log(err);
+    db.close();
   }
 };
 
