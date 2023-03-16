1fef5a3aa44e45eed9f27f95467d6b44c0c73de2
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-16 13:17:30 -0400

    add bookings table

---
+++ server/db/models/bookings.js
@@ -0,0 +1,74 @@
+const Sequelize = require('sequelize');
+const db = require('../database');
+
+// Sitter.hasMany(Booking);
+// Booking.belongsTo(Sitter);
+// User.hasMany(Booking);
+// Booking.belongsTo(User);
+// Payment.hasMany(Booking);
+// Booking.belongsTo(Payment);
+// Pet.belongsToMany(Booking);
+// Booking.belongsToMany(Pet);
+
+const Booking = db.define('booking', {
+  status: {
+    type: Sequelize.ENUM({
+      values: ['pending', 'approved', 'cancelled', 'complete'],
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        notNull: true,
+      },
+    }),
+  },
+  startDate: {
+    type: Sequelize.DATEONLY,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      notNull: true,
+    },
+  },
+  endDate: {
+    type: Sequelize.DATEONLY,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      notNull: true,
+    },
+  },
+  totalDays: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      notNull: true,
+    },
+  },
+  rate: {
+    type: Sequelize.DECIMAL(10, 2),
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      notNull: true,
+    },
+  },
+  totalAmount: {
+    type: Sequelize.DECIMAL(10, 2),
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      notNull: true,
+    },
+  },
+  location: {
+    type: Sequelize.ENUM('sitter', 'owner'),
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      notNull: true,
+    },
+  },
+});
+
+module.exports = Booking;
