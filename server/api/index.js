ef18e9f0129ed76e5aecc8af41bbbfce9c5e1ba2
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-19 17:16:41 -0400

    update typo on index

--- server/api/index.js
+++ server/api/index.js
@@ -3,14 +3,15 @@ const router = require('express').Router();
 // routes to specific pages
 router.use('/users', require('./routes/usersRoutes'));
 router.use('/pets', require('./routes/petsRoute'));
-router.use('/sitters', require('./routes/sitters'));
-// // reviews and ratings under sitters
-// router.use('/bookings', require('./routes/booking'));
-// router.use('/events', require('./routes/events'));
-// router.use('/posts', require('./routes/posts'));
-// router.use('/groups', require('./routes/groups'));
+router.use('/sitters', require('./routes/sittersRoute'));
+// router.use('/bookings', require('./routes/bookingRoutes'));
+// router.use('/events', require('./routes/eventsRoutes'));
+// router.use('/posts', require('./routes/postsRoutes'));
+// router.use('/groups', require('./routes/groupsRoutes'));
 router.use('/auth', require('./routes/authRoutes'));
-// router.use('/map', require('./routes/map'));
+// router.use('/map', require('./routes/mapRoutes'));
+// router.use('/reviews', require('./routes/reviewsRoutes'));
+// router.use('/ratings', require('./routes/ratingsRoutes'));
 
 // 404 default error handler
 router.use((req, res, next) => {
