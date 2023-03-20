38bcf3aadc030cabf943eff3056179c0c82ebe70
Allyssa Deorocki <55514548+allyssad@users.noreply.github.com>
2023-03-20 13:42:53 -0400

    Update index.js
    edit booking route name, add message route path

--- server/api/index.js
+++ server/api/index.js
@@ -7,11 +7,12 @@ router.use('/sitters', require('./routes/sittersRoute'));
 router.use('/events', require('./routes/eventsRoutes'));
 router.use('/posts', require('./routes/postsRoute'));
 router.use('/groups', require('./routes/groupsRoute'));
-router.use('/bookings', require('./routes/booking'));
+router.use('/bookings', require('./routes/bookingRoutes'));
 router.use('/auth', require('./routes/authRoutes'));
 // router.use('/map', require('./routes/mapRoutes'));
 // router.use('/reviews', require('./routes/reviewsRoutes'));
 // router.use('/ratings', require('./routes/ratingsRoutes'));
+// router.use('/messages', require('./routes/messagesRoutes'));
 
 // 404 default error handler
 router.use((req, res, next) => {
