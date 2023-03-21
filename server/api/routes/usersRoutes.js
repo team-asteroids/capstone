42bd21e7b891013e4f8c838e5e97dd1d42466521
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-21 16:00:00 -0400

    add access route paths to index and user file

--- server/api/routes/usersRoutes.js
+++ server/api/routes/usersRoutes.js
@@ -7,6 +7,7 @@ router.use('/:id/bookings', require('./bookingsRoutes'));
 router.use('/:id/events', require('./eventsRoutes'));
 router.use('/:id/reviews', require('./reviewsRoutes'));
 router.use('/:id/ratings', require('./ratingsRoutes'));
+router.use('/:id/access', require('./accessRoutes'));
 
 // Get all users
 router.get('/', async (req, res, next) => {
