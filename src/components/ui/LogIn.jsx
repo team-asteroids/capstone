027c7ca929be60632467268ff7bf06881905d521
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-22 13:00:24 -0400

    add sign up link to login page

--- src/components/ui/LogIn.jsx
+++ src/components/ui/LogIn.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function LogIn() {
-  return <div>LogIn</div>;
+  return (
+    <div>
+      <p>Don't have an account?</p>
+      <p>
+        <Link to="/signup">Sign Up!</Link>
+      </p>
+    </div>
+  );
 }
 
 export default LogIn;
