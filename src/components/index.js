5a843ae51ea0690a892cd8b9c181b8fdfe6ab1ed
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-22 11:32:54 -0400

    update file structure

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
 export { default as App } from './App.jsx';
-export { default as Homepage } from './Homepage.jsx';
-export { default as Navbar } from './Navbar.jsx';
-export { default as NotFound } from './NotFound';
+export { default as Homepage } from './ui/Homepage.jsx';
+export { default as Navbar } from './ui/Navbar.jsx';
+export { default as NotFound } from './ui/NotFound';
