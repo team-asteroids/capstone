b121d241f1f8724a2300ffacaa35d5d338fa0b88
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-22 16:10:16 -0400

    add logout reducer and exports

--- src/slices/authSlice.js
+++ src/slices/authSlice.js
@@ -1,8 +1,3 @@
-// LOGGED IN USER INFO
-// axios calls to auth routes
-// saving tokens and user object
-// this state to save anything to the user object
-// user === logged in person doing actions
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
@@ -32,21 +27,33 @@ const authSlice = createSlice({
       state.status = '';
       state.error = '';
     },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(logIn.fulfilled, (state, { payload }) => {
-        state.token = payload.token;
-        state.status = 'success';
-        state.error = '';
-      })
-      .addCase(logIn.pending, (state, { payload }) => {
-        state.status = 'loading';
-        state.error = '';
-      })
-      .addCase(logIn.rejected, (state, { payload }) => {
-        state.status = 'failed';
-        state.error = payload.message;
-      });
+    logOut: (state) => {
+      state.auth = {};
+      state.token = '';
+      state.status = '';
+      localStorage.clear();
+    },
+    extraReducers: (builder) => {
+      builder
+        .addCase(logIn.fulfilled, (state, { payload }) => {
+          state.token = payload.token;
+          state.status = 'success';
+          state.error = '';
+        })
+        .addCase(logIn.pending, (state, { payload }) => {
+          state.status = 'loading';
+          state.error = '';
+        })
+        .addCase(logIn.rejected, (state, { payload }) => {
+          state.status = 'failed';
+          state.error = payload.message;
+        });
+    },
   },
 });
+
+export const { resetAuthStatus, logOut } = authSlice.actions;
+
+export const selectAuth = (state) => state.auth;
+
+export default authSlice.reducer;
