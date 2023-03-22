9fb32c54802d692c47adbe6827ee383f99c270fa
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-22 17:02:43 -0400

    add log in auth flow

--- src/components/ui/LogIn.jsx
+++ src/components/ui/LogIn.jsx
@@ -1,23 +1,109 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import {
+  selectAuth,
+  logIn,
+  attemptTokenLogin,
+  // resetAuthStatus,
+} from '../../slices/authSlice';
 
 function LogIn() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  // local state from form (user-entered data)
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // state to confirm login success
+  // const [logInFail, setLogInFail] = useState(false);
+  // const [logInAttempt, setLogInAttempt] = useState(false);
+  // const [isInvalidEmail, setIsInvalidEmail] = useState(false);
+  // const [isInvalidPassword, setIsInvalidPassword] = useState(false);
+
+  const { userAuth } = useSelector(selectAuth);
+
+  console.log('userAuth:', userAuth);
+
+  // const validateEmail = (email) => {
+  //   let res = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  //   return res.test(email);
+  // };
+
+  // const checkFormValidation = () => {
+  //   if (email === '') {
+  //     setIsInvalidEmail(true);
+  //   }
+  //   if (!validateEmail(email)) {
+  //     setIsInvalidEmail(true);
+  //   }
+  //   if (password === '' || password.length < 8) {
+  //     setIsInvalidPassword(true);
+  //   }
+  // };
+
+  const attemptLogIn = async (evt) => {
+    evt.preventDefault();
+    // checkFormValidation();
+    // setLogInAttempt(true);
+
+    await dispatch(
+      logIn({
+        email,
+        password,
+      })
+    );
+
+    await dispatch(attemptTokenLogin());
+  };
+
+  useEffect(() => {
+    if (userAuth.firstName) navigate('/');
+    // else if (logInAttempt) {
+    //   setLogInFail(true);
+    // }
+
+    // return () => {
+    //   dispatch(resetAuthStatus());
+    // };
+  }, [userAuth]);
+
   return (
     <div className="bg-[url('img/dog-head.jpg')] bg-bottom bg-no-repeat bg-cover h-[calc(100vh_-_5rem)]">
       <div className="w-full flex flex-col max-w-sm m-auto pt-16">
         <h2 className="text-center text-3xl">Log in!</h2>
         <section className="flex gap-10 justify-center mt-16">
-          <form>
+          <form onSubmit={attemptLogIn}>
             <div className="flex flex-col gap-10">
               <div className="w-full">
                 <label>Email</label>
-                <input className="appearance-none block w-full bg-white-200 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-bold-blue mt-3" />
+                <input
+                  className="appearance-none block w-full bg-white-200 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-bold-blue mt-3"
+                  id="email"
+                  name="email"
+                  value={email}
+                  onChange={(evt) => {
+                    // setIsInvalidEmail(false);
+                    // setLogInFail(false);
+                    setEmail(evt.target.value);
+                  }}
+                />
               </div>
               <div className="w-full">
                 <label>Password</label>
                 <input
                   className="appearance-none block w-full bg-white-200 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-bold-blue mt-3"
                   type="password"
+                  id="password"
+                  name="password"
+                  placeholder="************"
+                  value={password}
+                  onChange={(evt) => {
+                    // setIsInvalidPassword(false);
+                    // setLogInFail(false);
+                    setPassword(evt.target.value);
+                  }}
                 />
               </div>
             </div>
--- src/slices/authSlice.js
+++ src/slices/authSlice.js
@@ -7,7 +7,26 @@ export const logIn = createAsyncThunk(
     try {
       let { data } = await axios.post('/api/auth/login', credentials);
       localStorage.setItem('token', data.token);
+      console.log('login data:', data);
       return data;
+    } catch (err) {
+      return rejectWithValue(err);
+    }
+  }
+);
+
+export const attemptTokenLogin = createAsyncThunk(
+  'attemptTokenLogin',
+  async (x, { rejectWithValue }) => {
+    try {
+      const token = localStorage.getItem('token');
+      if (!token) return {};
+      const { data } = await axios.get(`/api/auth`, {
+        headers: {
+          authorization: token,
+        },
+      });
+      return { data, token };
     } catch (err) {
       return rejectWithValue(err);
     }
@@ -20,7 +39,7 @@ const authSlice = createSlice({
     token: '',
     userAuth: {},
     error: '',
-    status: '', // pending, fulfilled, failed
+    status: '',
   },
   reducers: {
     resetAuthStatus: (state) => {
@@ -28,27 +47,41 @@ const authSlice = createSlice({
       state.error = '';
     },
     logOut: (state) => {
-      state.auth = {};
+      state.userAuth = {};
       state.token = '';
       state.status = '';
       localStorage.clear();
     },
-    extraReducers: (builder) => {
-      builder
-        .addCase(logIn.fulfilled, (state, { payload }) => {
-          state.token = payload.token;
-          state.status = 'success';
-          state.error = '';
-        })
-        .addCase(logIn.pending, (state, { payload }) => {
-          state.status = 'loading';
-          state.error = '';
-        })
-        .addCase(logIn.rejected, (state, { payload }) => {
-          state.status = 'failed';
-          state.error = payload.message;
-        });
-    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(logIn.fulfilled, (state, { payload }) => {
+        state.token = payload.token;
+        state.status = 'success';
+        state.error = '';
+      })
+      .addCase(logIn.pending, (state, { payload }) => {
+        state.status = 'loading';
+        state.error = '';
+      })
+      .addCase(logIn.rejected, (state, { payload }) => {
+        state.status = 'failed';
+        state.error = payload.message;
+      })
+      .addCase(attemptTokenLogin.fulfilled, (state, { payload }) => {
+        state.userAuth = payload.data;
+        state.status = 'success';
+        state.error = '';
+        state.token = payload.token;
+      })
+      .addCase(attemptTokenLogin.pending, (state, { payload }) => {
+        state.status = 'loading';
+        state.error = '';
+      })
+      .addCase(attemptTokenLogin.rejected, (state, { payload }) => {
+        state.status = 'failed';
+        state.error = payload.message;
+      });
   },
 });
 
