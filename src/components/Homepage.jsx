a60e6e2c2605986b6ffb4c8a4c07b3ad50ccfe24
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-22 10:26:01 -0400

    edit text

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -5,7 +5,7 @@ function Homepage() {
     <div className="bg-cover bg-center bg-[url('img/treats-bg.jpg')] h-screen">
       <div className="absolute flex-col text-[3.5rem] top-96 left-48 tracking-wide">
         <p className="leading-none">Join the Howlr Pack!</p>
-        <p className="leading-none">Aaawhoooo!</p>
+        <p className="leading-none">Aaawwoooo!</p>
       </div>
       <div className="flex absolute top-[35rem] text-2xl left-48">
         <p>Find Sitters, Friends, Packmates</p>
