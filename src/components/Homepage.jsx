da5f631e766a1beb62c698fd3ce3f443d1468b1a
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-22 10:52:22 -0400

    update homepage text

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,8 +4,8 @@ function Homepage() {
   return (
     <div className="bg-cover bg-no-repeat bg-[url('img/treats-bg.jpg')] h-[calc(100vh_-_5rem)]">
       <div className="absolute flex-col text-[3.5rem] top-96 left-48 tracking-wide">
-        <p className="leading-none">Welcome to the Howlr Pack</p>
-        <p className="leading-none">Aaawwoooo!</p>
+        <p className="leading-none pb-2">Welcome to the</p>
+        <p className="leading-none">Howlr Pack!</p>
       </div>
       <div className="flex absolute top-[35rem] text-2xl left-48">
         <p>Digital dog park for Sitters, Friends, Packmates</p>
