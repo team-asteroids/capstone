0c1f14bf1e08dd7aab86bc2d94d6c964d6534d0c
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-22 10:10:08 -0400

    add colors and font style to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,css}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rubik: ['Rubik Mono One', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif'],
+      },
+      fontWeight: {
+        thin: 100,
+        light: 300,
+        regular: 400,
+        medium: 500,
+        bold: 700,
+        black: 900,
+      },
+      colors: {
+        'bold-yellow': '#ffbe0b',
+        'bold-orange': '#fb5607',
+        'bold-pink': '#ff006e',
+        'bold-purple': '#8338ec',
+        'bold-blue': '#3a86ff',
+        // neutral: '#e9cecd',
+        'white-smoke': '#F2F4F3',
+      },
+    },
   },
   plugins: [],
 };
