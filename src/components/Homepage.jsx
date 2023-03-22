1287b499eb45cac2ba02aa8139669306666d09b6
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-22 10:14:46 -0400

    add homepage text

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 function Homepage() {
   return (
     <div className="bg-cover bg-center bg-[url('img/treats-bg.jpg')] h-screen">
-      Homepage
+      <div className="absolute flex-col text-[3.5rem] top-96 left-48 tracking-wide">
+        <p className="leading-none">Join the Howlr Pack!</p>
+        <p className="leading-none">Aaawhoooo!</p>
+      </div>
+      <div className="flex absolute top-[35rem] text-2xl left-48">
+        <p>Find Sitters, Friends, Packmates</p>
+      </div>
     </div>
   );
 }
