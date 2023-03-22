05bc76e837fd5dd02c2b837f132d3d797e9950e8
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-22 09:59:59 -0400

    test tailwind background to homepage component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import '../index.css';
 
 function App() {
   return (
-    <div className="h-screen bg-blue-200">
+    <div className="bg-cover bg-center bg-[url('img/treats-bg.jpg')] h-screen">
       <p className="underline text-3xl">Hello World!</p>
     </div>
   );
