8a9a12950b3a1279441bcb9420ec2d194cf68713
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-22 13:00:36 -0400

    add sign up form and initial styling

--- src/components/ui/SignUp.jsx
+++ src/components/ui/SignUp.jsx
@@ -2,7 +2,34 @@ import React from 'react';
 
 function SignUp() {
   return (
-    <div className="bg-bold-orange bg-cover bg-no-repeat h-[calc(100vh_-_5rem)]"></div>
+    <div className="bg-bold-orange h-[calc(100vh_-_5rem)]">
+      <div className="flex flex-row">
+        <div className="w-1/2">
+          <h2 className="text-center text-3xl font-bold">Welcome, New Pup!</h2>
+          <section className="flex gap-10 justify-center mt-16 ">
+            <form>
+              <div className="flex flex-wrap mx-3 mb-6">
+                <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                  <label>First Name</label>
+                  <input className="appearance-none block w-full bg-white-200 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-bold-orange" />
+                </div>
+                <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                  <label>Last Name</label>
+                  <input className="appearance-none block w-full bg-white-200 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-bold-orange" />
+                </div>
+              </div>
+              <div className="flex flex-wrap px-3 mx-3 mb-6">
+                <div className="w-full flex flex-col">
+                  <label>Email</label>
+                  <input className="appearance-none block w-full bg-white-200 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-bold-orange" />
+                </div>
+              </div>
+            </form>
+          </section>
+        </div>
+        <div className="w-1/2 bg-[url('img/signup-bg.jpg')] h-[calc(100vh_-_5rem)] bg-contain bg-center bg-no-repeat"></div>
+      </div>
+    </div>
   );
 }
 
