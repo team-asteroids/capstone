5542142d3831168ee0bad39cdc8c2027792351c2
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-22 10:48:03 -0400

    edit homepage style

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 function Homepage() {
   return (
-    <div className="bg-cover bg-center bg-[url('img/treats-bg.jpg')] h-screen">
+    <div className="bg-cover bg-no-repeat bg-[url('img/treats-bg.jpg')] h-[calc(100vh_-_5rem)]">
       <div className="absolute flex-col text-[3.5rem] top-96 left-48 tracking-wide">
-        <p className="leading-none">Join the Howlr Pack!</p>
+        <p className="leading-none">Welcome to the Howlr Pack</p>
         <p className="leading-none">Aaawwoooo!</p>
       </div>
       <div className="flex absolute top-[35rem] text-2xl left-48">
-        <p>Find Sitters, Friends, Packmates</p>
+        <p>Digital dog park for Sitters, Friends, Packmates</p>
       </div>
     </div>
   );
