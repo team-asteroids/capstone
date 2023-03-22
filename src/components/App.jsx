d52bd7ced91ecaa7880229529b230acc690bb457
Forhad Zinnah <forhad@Forhads-Mac-mini.local>
2023-03-22 16:48:22 -0700

    event details component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,15 @@
 import '../index.css';
 import { Routes, Route } from 'react-router-dom';
-import { Homepage, Navbar, NotFound, SignUp, LogIn, AllEvents, Map } from './index';
-
+import {
+  Homepage,
+  Navbar,
+  NotFound,
+  SignUp,
+  LogIn,
+  AllEvents,
+  Map,
+  EventDetails,
+} from './index';
 
 function App() {
   return (
@@ -12,6 +20,7 @@ function App() {
         <Route path="/*" element={<NotFound />} />
         <Route path="/map" element={<Map />} />
         <Route path="/events" element={<AllEvents />} />
+        <Route path="/events/:id" element={<EventDetails />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<LogIn />} />
       </Routes>
--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,6 @@ export { default as Navbar } from './ui/Navbar.jsx';
 export { default as NotFound } from './ui/NotFound';
 export { default as Map } from './ui/Map.jsx';
 export { default as AllEvents } from './events/AllEvents.jsx';
+export { default as EventDetails } from './events/EventDetails';
 export { default as SignUp } from './ui/SignUp.jsx';
 export { default as LogIn } from './ui/LogIn.jsx';
