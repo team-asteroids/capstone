aa839dc7bb1b4701114863f8553c01b2716c22a5
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-22 15:14:39 -0400

    add changes

--- src/components/ui/LogIn.jsx
+++ src/components/ui/LogIn.jsx
@@ -3,11 +3,40 @@ import { Link } from 'react-router-dom';
 
 function LogIn() {
   return (
-    <div>
-      <p>Don't have an account?</p>
-      <p>
-        <Link to="/signup">Sign Up!</Link>
-      </p>
+    <div className="bg-[url('img/dog-head.jpg')] bg-bottom bg-no-repeat bg-cover h-[calc(100vh_-_5rem)]">
+      <div className="w-full max-w-sm m-auto pt-16">
+        <h2 className="text-center text-3xl">Log in!</h2>
+        <section className="flex gap-10 justify-center mt-16">
+          <form>
+            <div className="flex flex-col gap-10">
+              <div className="w-full">
+                <label>Email</label>
+                <input className="appearance-none block w-full bg-white-200 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-bold-blue mt-3" />
+              </div>
+              <div className="w-full">
+                <label>Password</label>
+                <input
+                  className="appearance-none block w-full bg-white-200 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-bold-blue mt-3"
+                  type="password"
+                />
+              </div>
+            </div>
+            <div>
+              <button
+                className="ease-in duration-500  hover:bg-primary-button-hover w-full bg-primary-deep-green text-white py-2 rounded-xl mx-auto block text-xl hover:transition-all mt-10"
+                type="submit"
+              >
+                Log in
+              </button>
+            </div>
+          </form>
+        </section>
+        <div className="text-xs mt-5">
+          <p>
+            <Link to={'/signup'}>Don't have an account? Sign Up!</Link>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
