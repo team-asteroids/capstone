01f389595b9d1789ab72bc4174037b44bdaa9554
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-22 17:52:54 -0400

    add login validations

--- src/components/ui/LogIn.jsx
+++ src/components/ui/LogIn.jsx
@@ -5,7 +5,7 @@ import {
   selectAuth,
   logIn,
   attemptTokenLogin,
-  // resetAuthStatus,
+  resetAuthStatus,
 } from '../../slices/authSlice';
 
 function LogIn() {
@@ -17,99 +17,137 @@ function LogIn() {
   const [password, setPassword] = useState('');
 
   // state to confirm login success
-  // const [logInFail, setLogInFail] = useState(false);
-  // const [logInAttempt, setLogInAttempt] = useState(false);
-  // const [isInvalidEmail, setIsInvalidEmail] = useState(false);
-  // const [isInvalidPassword, setIsInvalidPassword] = useState(false);
+  const [logInFail, setLogInFail] = useState(false);
+  const [logInAttempt, setLogInAttempt] = useState(false);
+  const [isInvalidEmail, setIsInvalidEmail] = useState(false);
+  const [isInvalidPassword, setIsInvalidPassword] = useState(false);
 
-  const { userAuth } = useSelector(selectAuth);
+  const { userAuth, status } = useSelector(selectAuth);
 
-  console.log('userAuth:', userAuth);
+  const validClass =
+    'appearance-none block w-full bg-white-200 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-bold-blue mt-3 font-rubik';
 
-  // const validateEmail = (email) => {
-  //   let res = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  //   return res.test(email);
-  // };
+  const invalidClass =
+    'appearance-none block border border-red-500 w-full bg-white-200 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-bold-blue mt-3 font-rubik';
 
-  // const checkFormValidation = () => {
-  //   if (email === '') {
-  //     setIsInvalidEmail(true);
-  //   }
-  //   if (!validateEmail(email)) {
-  //     setIsInvalidEmail(true);
-  //   }
-  //   if (password === '' || password.length < 8) {
-  //     setIsInvalidPassword(true);
-  //   }
-  // };
+  const validateEmail = (email) => {
+    let res = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    return res.test(email);
+  };
+
+  const checkFormValidation = () => {
+    if (email === '') {
+      setIsInvalidEmail(true);
+    }
+    if (!validateEmail(email)) {
+      setIsInvalidEmail(true);
+    }
+    if (password === '' || password.length < 8) {
+      setIsInvalidPassword(true);
+    }
+  };
 
   const attemptLogIn = async (evt) => {
     evt.preventDefault();
-    // checkFormValidation();
-    // setLogInAttempt(true);
-
-    await dispatch(
-      logIn({
-        email,
-        password,
-      })
-    );
-
-    await dispatch(attemptTokenLogin());
+    checkFormValidation();
+    setLogInAttempt(true);
+    if (!isInvalidEmail && !isInvalidPassword) {
+      const res = await dispatch(
+        logIn({
+          email,
+          password,
+        })
+      );
+      console.log(res);
+      if (res.type === 'login/fulfilled') {
+        await dispatch(attemptTokenLogin());
+      }
+    }
   };
 
   useEffect(() => {
-    if (userAuth.firstName) navigate('/');
-    // else if (logInAttempt) {
-    //   setLogInFail(true);
-    // }
+    if (userAuth && userAuth.firstName) navigate('/');
+    else if (logInAttempt) {
+      setLogInFail(true);
+    }
 
-    // return () => {
-    //   dispatch(resetAuthStatus());
-    // };
+    return () => {
+      dispatch(resetAuthStatus());
+    };
   }, [userAuth]);
 
   return (
     <div className="bg-[url('img/dog-head.jpg')] bg-bottom bg-no-repeat bg-cover h-[calc(100vh_-_5rem)]">
       <div className="w-full flex flex-col max-w-sm m-auto pt-16">
         <h2 className="text-center text-3xl">Log in!</h2>
-        <section className="flex gap-10 justify-center mt-16">
-          <form onSubmit={attemptLogIn}>
-            <div className="flex flex-col gap-10">
+        <section className="flex gap-10 justify-center pt-5">
+          <form onSubmit={attemptLogIn} className="w-full">
+            <div className="flex flex-col gap-5">
               <div className="w-full">
+                <p
+                  className={
+                    status === 'failed' || logInAttempt
+                      ? 'text-red-500 font-rubik text-center font-bold text-sm'
+                      : 'collapse font-rubik text-xs'
+                  }
+                >
+                  Invalid login credentials!
+                </p>
                 <label>Email</label>
                 <input
-                  className="appearance-none block w-full bg-white-200 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-bold-blue mt-3"
+                  className={
+                    isInvalidEmail || logInFail ? invalidClass : validClass
+                  }
                   id="email"
                   name="email"
                   value={email}
                   onChange={(evt) => {
-                    // setIsInvalidEmail(false);
-                    // setLogInFail(false);
+                    setIsInvalidEmail(false);
+                    setLogInFail(false);
                     setEmail(evt.target.value);
                   }}
                 />
+                <p
+                  className={
+                    isInvalidEmail
+                      ? 'text-red-500 font-rubik text-sm font-bold'
+                      : 'collapse font-rubik text-xs'
+                  }
+                >
+                  Invalid email!
+                </p>
               </div>
               <div className="w-full">
                 <label>Password</label>
                 <input
-                  className="appearance-none block w-full bg-white-200 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-bold-blue mt-3"
+                  className={
+                    isInvalidPassword || logInFail ? invalidClass : validClass
+                  }
                   type="password"
                   id="password"
                   name="password"
                   placeholder="************"
                   value={password}
                   onChange={(evt) => {
-                    // setIsInvalidPassword(false);
-                    // setLogInFail(false);
+                    setIsInvalidPassword(false);
+                    setLogInFail(false);
                     setPassword(evt.target.value);
                   }}
                 />
+                <p
+                  className={
+                    isInvalidPassword
+                      ? 'text-red-500 font-rubik text-sm font-bold'
+                      : 'collapse font-rubik text-xs'
+                  }
+                >
+                  Invalid password!
+                </p>
               </div>
             </div>
             <div>
               <button
-                className="ease-in duration-500  hover:bg-primary-button-hover w-full bg-primary-deep-green text-white py-2 rounded-xl mx-auto block text-xl hover:transition-all mt-10"
+                className="ease-in duration-500 hover:bg-primary-button-hover w-full bg-primary-deep-green text-white py-2 rounded-xl mx-auto block text-xl hover:transition-all mt-10"
                 type="submit"
               >
                 Log in
