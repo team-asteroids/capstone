c75b0327f7e9e9b4aa5c0fd8744f32bb1adbe006
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-22 13:40:05 -0400

    add additional inputs

--- src/components/ui/SignUp.jsx
+++ src/components/ui/SignUp.jsx
@@ -4,7 +4,7 @@ function SignUp() {
   return (
     <div className="bg-[url('img/signup-blue.jpg')] bg-no-repeat bg-cover bg-right h-[calc(100vh_-_5rem)]">
       <div className="flex flex-row">
-        <div className="w-1/2 m-auto">
+        <div className="w-1/2 mx-10 m-auto">
           <h2 className="text-center text-3xl font-bold">Welcome, New Pup!</h2>
           <section className="flex gap-10 justify-center mt-16">
             <form>
@@ -22,6 +22,33 @@ function SignUp() {
                 <div className="w-full flex flex-col">
                   <label>Email</label>
                   <input className="appearance-none block w-full bg-white-200 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-bold-blue mt-3" />
+                </div>
+              </div>
+              <div className="flex flex-wrap mx-3 mb-6">
+                <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                  <label>Zip Code</label>
+                  <input
+                    className="appearance-none block w-full bg-white-200 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-bold-blue mt-3"
+                    type="text"
+                  />
+                </div>
+                <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                  <label>Pets</label>
+                  <input
+                    className="appearance-none block w-full bg-white-200 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-bold-blue mt-3"
+                    type="number"
+                    min={0}
+                  />
+                </div>
+                <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                  <label>Foster</label>
+                  <select className="appearance-none block w-full bg-white-200 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-bold-blue mt-3">
+                    {/* <option value="" selected disabled hidden>
+                      Can update later
+                    </option> */}
+                    <option>Yes</option>
+                    <option>No</option>
+                  </select>
                 </div>
               </div>
               <div>
