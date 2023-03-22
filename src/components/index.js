3eede1742bede2ce54fea0bf60c4461fee4f5a02
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-22 11:01:52 -0400

    add signup and login component skeletons

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@ export { default as App } from './App.jsx';
 export { default as Homepage } from './Homepage.jsx';
 export { default as Navbar } from './Navbar.jsx';
 export { default as NotFound } from './NotFound';
+export { default as SignUp } from './SignUp.jsx';
+export { default as LogIn } from './LogIn';
