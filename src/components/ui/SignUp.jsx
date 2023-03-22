cc695ff059bbef4bf375c861333f1541d0422947
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-22 13:24:16 -0400

    add sign up form

--- src/components/ui/SignUp.jsx
+++ src/components/ui/SignUp.jsx
@@ -2,27 +2,35 @@ import React from 'react';
 
 function SignUp() {
   return (
-    <div className="bg-[url('img/signup-blue.jpg')] bg-no-repeat bg-cover bg-left h-[calc(100vh_-_5rem)]">
+    <div className="bg-[url('img/signup-blue.jpg')] bg-no-repeat bg-cover bg-right h-[calc(100vh_-_5rem)]">
       <div className="flex flex-row">
-        <div className="w-1/2">
+        <div className="w-1/2 m-auto">
           <h2 className="text-center text-3xl font-bold">Welcome, New Pup!</h2>
-          <section className="flex gap-10 justify-center mt-16 ">
+          <section className="flex gap-10 justify-center mt-16">
             <form>
               <div className="flex flex-wrap mx-3 mb-6">
                 <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                   <label>First Name</label>
-                  <input className="appearance-none block w-full bg-white-200 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-bold-orange" />
+                  <input className="appearance-none block w-full bg-white-200 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-bold-blue" />
                 </div>
                 <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                   <label>Last Name</label>
-                  <input className="appearance-none block w-full bg-white-200 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-bold-orange" />
+                  <input className="appearance-none block w-full bg-white-200 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-bold-blue" />
                 </div>
               </div>
               <div className="flex flex-wrap px-3 mx-3 mb-6">
                 <div className="w-full flex flex-col">
                   <label>Email</label>
-                  <input className="appearance-none block w-full bg-white-200 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-bold-orange" />
+                  <input className="appearance-none block w-full bg-white-200 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-bold-blue" />
                 </div>
+              </div>
+              <div>
+                <button
+                  className="ease-in duration-500  hover:bg-primary-button-hover w-full bg-primary-deep-green text-white py-2 rounded-xl mx-auto block text-xl hover:transition-all mt-10"
+                  type="submit"
+                >
+                  Sign Up
+                </button>
               </div>
             </form>
           </section>
