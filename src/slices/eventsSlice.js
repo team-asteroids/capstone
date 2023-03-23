6669d9702f84c3a05a897357a38b8ff02c12e59c
Forhad Zinnah <forhad@Forhads-Mac-mini.local>
2023-03-23 12:46:01 -0700

    event details started

--- src/slices/eventsSlice.js
+++ src/slices/eventsSlice.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 export const fetchAllEvents = createAsyncThunk('/getAllEvents', async () => {
   const { data } = await axios.get('/api/events');
+
   return data;
 });
 
@@ -10,11 +11,26 @@ export const fetchSingleEvent = createAsyncThunk(
   '/getEvent',
   async (eventId) => {
     const { data } = await axios.get(`/api/events/${eventId}`);
+
     return data;
   }
 );
 
-//_____________SLICE________________//
+export const addRsvpAsync = createAsyncThunk('/addRSVP', async (eventId) => {
+  const token = window.localStorage.getItem('token');
+  const { data } = await axios.post(
+    '/api/events/attending',
+    { eventId: eventId },
+    {
+      headers: {
+        authorization: token,
+      },
+    }
+  );
+  console.log('inside thunk', data);
+  return data;
+});
+
 export const eventsSlice = createSlice({
   name: 'events',
   initialState: {
@@ -51,11 +67,24 @@ export const eventsSlice = createSlice({
       .addCase(fetchSingleEvent.rejected, (state, { payload }) => {
         state.status = 'failed';
         state.error = payload.message;
+      })
+      .addCase(addRsvpAsync.fulfilled, (state, { payload }) => {
+        state.status = 'fulfilled';
+        state.error = '';
+        return;
+      })
+      .addCase(addRsvpAsync.pending, (state, { payload }) => {
+        state.status = 'loading';
+        state.error = '';
+      })
+      .addCase(addRsvpAsync.rejected, (state, { payload }) => {
+        state.status = 'failed';
+        state.error = payload.message;
       });
   },
 });
 
 export const selectEvents = (state) => state.events;
-export const selectSingleEvent = (state) => state.singleEvent;
+export const selectSingleEvent = (state) => state.events.singleEvent;
 
 export default eventsSlice.reducer;
