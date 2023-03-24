917c7255aa75afed633d85b6762e1ba308c33a9f
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-24 13:09:07 -0400

    add fetch all and single user thunks

--- src/slices/usersSlice.js
+++ src/slices/usersSlice.js
@@ -1,6 +1,30 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+export const fetchAllUsers = createAsyncThunk(
+  'allUsers',
+  async (x, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.get('/api/users');
+      return data;
+    } catch (err) {
+      return rejectWithValue(err);
+    }
+  }
+);
+
+export const fetchSingleUser = createAsyncThunk(
+  'singleUser',
+  async (id, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.get(`/api/users/${id}`);
+      return data;
+    } catch (err) {
+      return rejectWithValue(err);
+    }
+  }
+);
+
 const userSlice = createSlice({
   name: 'users',
   initialState: {
@@ -15,7 +39,35 @@ const userSlice = createSlice({
       state.error = '';
     },
   },
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAllUsers.fulfilled, (state, { payload }) => {
+        state.allUsers = payload || [];
+        state.status = 'success';
+        state.error = '';
+      })
+      .addCase(fetchAllUsers.pending, (state, { payload }) => {
+        state.status = 'loading';
+        state.error = '';
+      })
+      .addCase(fetchAllUsers.rejected, (state, { payload }) => {
+        state.status = 'failed';
+        state.error = payload.message;
+      })
+      .addCase(fetchSingleUser.fulfilled, (state, { payload }) => {
+        state.singleUser = payload || {};
+        state.status = 'success';
+        state.error = '';
+      })
+      .addCase(fetchSingleUser.pending, (state, { payload }) => {
+        state.status = 'loading';
+        state.error = '';
+      })
+      .addCase(fetchSingleUser.rejected, (state, { payload }) => {
+        state.status = 'failed';
+        state.error = payload.message;
+      });
+  },
 });
 
 export const { resetUserStatus } = userSlice.actions;
