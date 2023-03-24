bc5d46e00f50cb30a005f2a490adf0df03eec0e1
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-24 15:01:19 -0400

    switch data pull from auth to singleuser state

--- src/components/profiles/UserProfile.jsx
+++ src/components/profiles/UserProfile.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, Routes, Route, useParams } from 'react-router-dom';
-import { selectAuth } from '../../slices/authSlice';
+// import { selectAuth } from '../../slices/authSlice';
 import defaultImg from '../../img/default-dog.jpg';
 import { fetchSingleUser, selectUser } from '../../slices/usersSlice';
 
@@ -12,16 +12,15 @@ function UserProfile() {
 
   const [sitterToggle, setSitterToggle] = useState(false);
 
-  const { userAuth } = useSelector(selectAuth);
   const { singleUser } = useSelector(selectUser);
 
   const { id } = location;
 
-  // console.log('singleUser:', singleUser);
-
   useEffect(() => {
-    dispatch(fetchSingleUser(id));
-  }, [userAuth]);
+    if (id) {
+      dispatch(fetchSingleUser(id));
+    }
+  }, [id]);
 
   const toggleClass =
     "checked w-14 h-7 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-pale-blue  rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all  peer-checked:bg-bold-pink";
@@ -35,57 +34,57 @@ function UserProfile() {
     }
   };
 
-  if (!userAuth.firstName)
+  if (!singleUser.firstName)
     return <div className="font-rubikmono">Fetching good things...</div>;
 
   return (
     <div className="bg-cover bg-no-repeat bg-[url('img/profile-bg.jpg')] h-[calc(100vh_-_5rem)]">
       <div className="flex flex-row pt-20 mb-16 gap-10 mx-20 px-20">
-        <div className="w-1/5 flex flex-col gap-5">
+        <div className="w-1/4 flex flex-col gap-5">
           <div id="userBio" className="w-full flex flex-col gap-5">
-            <div className="flex flex-col">
+            <div className="flex flex-col gap-5">
               <img
-                className="h-48 w-48 rounded-full drop-shadow-md mb-5"
+                className="h-48 w-48 rounded-full drop-shadow-md"
                 src={defaultImg}
                 alt="alt"
               ></img>
-              <div className="font-rubikmono mb-2">
-                <p>{userAuth.fullName}</p>
-              </div>
-              <div className="font-rubikmono">
-                <p>{singleUser.access.zip}</p>
-              </div>
+              <div className="font-rubikmono">{singleUser.fullName}</div>
+              {singleUser.role === 'sitter' ? (
+                <div>
+                  <label className="relative inline-flex items-center cursor-pointer">
+                    <input
+                      type="checkbox"
+                      className="sr-only peer"
+                      onClick={toggleSitter}
+                    />
+                    <div className={toggleClass}></div>
+                    <span className="ml-3 text-sm font-medium text-gray-900">
+                      Sitter Profile
+                    </span>
+                  </label>
+                </div>
+              ) : (
+                <div>
+                  <label className="relative inline-flex items-center cursor-pointer">
+                    <input type="checkbox" className="sr-only peer" disabled />
+                    <div className={toggleClass}></div>
+                    <span className="ml-3 text-sm font-medium text-gray-400">
+                      Sitter Profile
+                    </span>
+                  </label>
+                </div>
+              )}
             </div>
           </div>
-          {userAuth.role === 'sitter' ? (
-            <div>
-              <label className="relative inline-flex items-center cursor-pointer">
-                <input
-                  type="checkbox"
-                  className="sr-only peer"
-                  onClick={toggleSitter}
-                />
-                <div className={toggleClass}></div>
-                <span className="ml-3 text-sm font-medium text-gray-900">
-                  Sitter Profile
-                </span>
-              </label>
+          <div>
+            OTHER STUFF
+            <div className="">
+              <p>{singleUser.access.zip}</p>
             </div>
-          ) : (
-            <div>
-              <label className="relative inline-flex items-center cursor-pointer">
-                <input type="checkbox" className="sr-only peer" disabled />
-                <div className={toggleClass}></div>
-                <span className="ml-3 text-sm font-medium text-gray-400">
-                  Sitter Profile
-                </span>
-              </label>
-            </div>
-          )}
-          <div>OTHER STUFF</div>
+          </div>
         </div>
       </div>
-      <div className="w-4/5 font-rubikmono overflow-auto flex flex-col gap-5">
+      <div className="w-3/4 font-rubikmono overflow-auto flex flex-col gap-5">
         <Routes></Routes>
       </div>
     </div>
