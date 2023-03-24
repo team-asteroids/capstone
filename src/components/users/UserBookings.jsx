6e8fc3670acfb89ea7d249c4e4db0f7ce676018e
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-24 08:57:56 -0400

    add booking cards and filter button

--- src/components/users/UserBookings.jsx
+++ src/components/users/UserBookings.jsx
@@ -36,9 +36,183 @@ function UserBookings() {
   // map through bookings and add a filter for (pending, approved, completed, declined/cancelled/withdrawn)
 
   return (
-    <div>
-      <h2 className="font-rubikmono">Manage Bookings</h2>
-      {status === 'success' && userBookings.length > 0 ? <p>Bookings</p> : null}
+    <div className="flex flex-col gap-5 font-rubik">
+      <div id="filter" className="flex flex-row justify-between">
+        <h2 className="font-rubikmono">Manage Bookings</h2>
+        <div>
+          <select
+            id="filter"
+            class="bg-white-smoke border border-white-smoke text-gray-900 text-sm rounded-lg focus:ring-pale-blue focus:border-pale-blue block p-1.5 px-3 drop-shadow-sm"
+          >
+            <option selected disabled>
+              filter
+            </option>
+            <option value="all">all</option>
+            <option value="approved">approved</option>
+            <option value="pending">pending</option>
+            <option value="completed">completed</option>
+            <option value="cancelled">cancelled</option>
+          </select>
+        </div>
+      </div>
+      <div className="h-[calc(100vh_-_20rem)] overflow-auto flex flex-col gap-5">
+        <div id="bookings" className="flex flex-col gap-5" h-48 w-full>
+          <div className="flex flex-col gap-5">
+            {status === 'success' && userBookings.length > 0
+              ? userBookings.map((booking) => {
+                  return (
+                    <div
+                      className="bg-white-smoke drop-shadow-sm p-5 rounded-xl"
+                      key={booking.id}
+                      h-48
+                      w-full
+                    >
+                      <div>
+                        <h2>{booking.status}</h2>
+                        <p>
+                          {booking.startDate} - {booking.endDate}
+                        </p>
+                      </div>
+                      <div>
+                        <p>Sitter Details Placeholder</p>
+                        <p>${booking.rate}</p>
+                      </div>
+                      <div>
+                        <p>CTA Placeholder</p>
+                      </div>
+                      <div>
+                        <p>Pets Placeholder</p>
+                      </div>
+                    </div>
+                  );
+                })
+              : null}
+          </div>
+
+          <div
+            className="bg-white-smoke drop-shadow-sm p-5 rounded-xl"
+            h-48
+            w-full
+          >
+            <div>
+              <h2>status test</h2>
+              <p>startDate - endDate</p>
+            </div>
+            <div>
+              <p>Sitter Details Placeholder</p>
+              <p>Rate</p>
+            </div>
+            <div>
+              <p>CTA Placeholder</p>
+            </div>
+            <div>
+              <p>Pets Placeholder</p>
+            </div>
+          </div>
+
+          <div
+            className="bg-white-smoke drop-shadow-sm p-5 rounded-xl"
+            h-48
+            w-full
+          >
+            <div>
+              <h2>status</h2>
+              <p>startDate - endDate</p>
+            </div>
+            <div>
+              <p>Sitter Details Placeholder</p>
+              <p>$rate</p>
+            </div>
+            <div>
+              <p>CTA Placeholder</p>
+            </div>
+            <div>
+              <p>Pets Placeholder</p>
+            </div>
+          </div>
+
+          <div
+            className="bg-white-smoke drop-shadow-sm p-5 rounded-xl"
+            h-48
+            w-full
+          >
+            <div>
+              <h2>status</h2>
+              <p>startDate - endDate</p>
+            </div>
+            <div>
+              <p>Sitter Details Placeholder</p>
+              <p>$rate</p>
+            </div>
+            <div>
+              <p>CTA Placeholder</p>
+            </div>
+            <div>
+              <p>Pets Placeholder</p>
+            </div>
+          </div>
+          <div
+            className="bg-white-smoke drop-shadow-sm p-5 rounded-xl"
+            h-48
+            w-full
+          >
+            <div>
+              <h2>status</h2>
+              <p>startDate - endDate</p>
+            </div>
+            <div>
+              <p>Sitter Details Placeholder</p>
+              <p>$rate</p>
+            </div>
+            <div>
+              <p>CTA Placeholder</p>
+            </div>
+            <div>
+              <p>Pets Placeholder</p>
+            </div>
+          </div>
+          <div
+            className="bg-white-smoke drop-shadow-sm p-5 rounded-xl"
+            h-48
+            w-full
+          >
+            <div>
+              <h2>status</h2>
+              <p>startDate - endDate</p>
+            </div>
+            <div>
+              <p>Sitter Details Placeholder</p>
+              <p>$rate</p>
+            </div>
+            <div>
+              <p>CTA Placeholder</p>
+            </div>
+            <div>
+              <p>Pets Placeholder</p>
+            </div>
+          </div>
+          <div
+            className="bg-white-smoke drop-shadow-sm p-5 rounded-xl"
+            h-48
+            w-full
+          >
+            <div>
+              <h2>status</h2>
+              <p>startDate - endDate</p>
+            </div>
+            <div>
+              <p>Sitter Details Placeholder</p>
+              <p>$rate</p>
+            </div>
+            <div>
+              <p>CTA Placeholder</p>
+            </div>
+            <div>
+              <p>Pets Placeholder</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
