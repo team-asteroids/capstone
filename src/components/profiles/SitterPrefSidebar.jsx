f9c5d2fd1c32106ede2657105e5740757ad4adf9
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-24 16:20:14 -0400

    add sitter pref list

--- src/components/profiles/SitterPrefSidebar.jsx
+++ src/components/profiles/SitterPrefSidebar.jsx
@@ -1,7 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { fetchSingleSitter, selectSitters } from '../../slices/sittersSlice';
+import { selectUser } from '../../slices/usersSlice';
 
 function SitterPrefSidebar() {
-  return <div>SitterPrefSidebar</div>;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [sitterPrefs, setSitterPrefs] = useState({});
+
+  const { singleUser } = useSelector(selectUser);
+  const { singleSitter } = useSelector(selectSitters);
+
+  const id = singleUser.sitter.id;
+
+  useEffect(() => {
+    if (id) {
+      dispatch(fetchSingleSitter(id));
+    }
+  }, [id]);
+
+  useEffect(() => {
+    if (singleSitter.sitterPrefs) {
+      setSitterPrefs(singleSitter.sitterPrefs[0]);
+    }
+  }, [singleSitter]);
+
+  return (
+    <div>
+      <div className="font-rubikmono">Preferences</div>
+      <div>
+        <p>Location:</p>
+        <ul>
+          {sitterPrefs.hostAtHome ? <li>sitter home</li> : null}
+          {sitterPrefs.atOwnerHouse ? <li>owner home</li> : null}
+        </ul>
+      </div>
+      <div>
+        <p>Size:</p>
+        <ul>
+          {sitterPrefs.small ? <li>small</li> : null}
+          {sitterPrefs.medium ? <li>medium</li> : null}
+          {sitterPrefs.large ? <li>large</li> : null}
+          {sitterPrefs.extraLarge ? <li>extra large</li> : null}
+          {sitterPrefs.puppies ? <li>puppies</li> : null}
+        </ul>
+      </div>
+      <div>
+        <p>Other Pets:</p>
+        <ul>
+          {sitterPrefs.cats ? <li>cats</li> : null}
+          {sitterPrefs.multiplePets ? <li>multiple pets</li> : null}
+        </ul>
+      </div>
+      <div>
+        <p>Special Needs:</p>
+        <ul>
+          {sitterPrefs.disabled ? <li>disabled</li> : null}
+          {sitterPrefs.medication ? <li>medication</li> : null}
+          {sitterPrefs.reactive ? <li>reactive</li> : null}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default SitterPrefSidebar;
