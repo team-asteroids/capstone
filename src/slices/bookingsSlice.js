813eaa1c01a6157b849d2837b0d46fe526d1a46d
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-23 20:59:04 -0400

    add fetchallbookings thunk

--- src/slices/bookingsSlice.js
+++ src/slices/bookingsSlice.js
@@ -1,10 +1,28 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const bookingSlice = createSlice({
-  name: 'booking',
+export const fetchAllBookings = createAsyncThunk(
+  'allBookings',
+  async ({ id, token }, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.get(`/api/users/${id}/bookings`, {
+        headers: {
+          authorization: token,
+        },
+      });
+      // console.log(data);
+      return data;
+    } catch (err) {
+      return rejectWithValue(err);
+    }
+  }
+);
+
+const bookingsSlice = createSlice({
+  name: 'bookings',
   initialState: {
-    bookings: {},
+    allBookings: [],
+    singleBooking: {},
     status: '',
     error: '',
   },
@@ -14,11 +32,26 @@ const bookingSlice = createSlice({
       state.error = '';
     },
   },
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAllBookings.fulfilled, (state, { payload }) => {
+        state.allBookings = payload || [];
+        state.status = 'success';
+        state.error = '';
+      })
+      .addCase(fetchAllBookings.pending, (state, { payload }) => {
+        state.status = 'loading';
+        state.error = '';
+      })
+      .addCase(fetchAllBookings.rejected, (state, { payload }) => {
+        state.status = 'failed';
+        state.error = payload.message;
+      });
+  },
 });
 
-export const { resetBookingStatus } = bookingSlice.actions;
+export const { resetBookingStatus } = bookingsSlice.actions;
 
-export const selectBooking = (state) => state.booking;
+export const selectBookings = (state) => state.bookings;
 
-export default bookingSlice.reducer;
+export default bookingsSlice.reducer;
