ec66a8c87e097d6c02dfee05aa1ba13481ad4b6d
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-24 16:46:04 -0400

    add sitter profile sections

--- src/components/profiles/SitterProfile.jsx
+++ src/components/profiles/SitterProfile.jsx
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  fetchSingleSitter,
+  fetchSingleSitterRatings,
+  fetchSingleSitterReviews,
+  selectSitters,
+} from '../../slices/sittersSlice';
 
-function SitterProfile() {
+const SitterProfile = () => {
+  const dispatch = useDispatch();
+
+  const { singleSitter, sitterReviews, sitterRatings } =
+    useSelector(selectSitters);
+
+  const id = singleSitter.id;
+
+  useEffect(() => {
+    dispatch(fetchSingleSitterReviews(id));
+    dispatch(fetchSingleSitterRatings(id));
+  }, [singleSitter]);
+
+  // console.log(sitterRatings, sitterReviews);
+
   return (
     <div className="font-rubik flex flex-col gap-5">
       <div>
         <h2 className="font-rubikmono">Sitter Profile</h2>
       </div>
       <div className="h-[calc(100vh_-_20rem)] overflow-auto flex flex-col gap-5">
-        <div>Sitter Bio</div>
-        <div>Availability</div>
-        <div>Ratings & Reviews</div>
+        <div>
+          <h2 className="font-rubikmono">About Me</h2>
+          <p>{singleSitter.bio}</p>
+        </div>
+        <div>
+          <h2 className="font-rubikmono">Availability</h2>
+          <p>{singleSitter.bio}</p>
+        </div>
+        <div>
+          <h2 className="font-rubikmono">Ratings & Reviews</h2>
+          <p>{singleSitter.bio}</p>
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default SitterProfile;
