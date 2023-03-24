c029d78e83fdaa3e22c0705653d193f76cac6fb3
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-24 12:58:03 -0400

    add basic user slice

--- src/slices/usersSlice.js
+++ src/slices/usersSlice.js
@@ -1,1 +1,25 @@
-// pop all user stuff
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const userSlice = createSlice({
+  name: 'users',
+  initialState: {
+    allUsers: [],
+    singleUser: {},
+    status: '',
+    error: '',
+  },
+  reducers: {
+    resetUserStatus: (state) => {
+      state.status = '';
+      state.error = '';
+    },
+  },
+  extraReducers: (builder) => {},
+});
+
+export const { resetUserStatus } = userSlice.actions;
+
+export const selectUser = (state) => state.users;
+
+export default userSlice.reducer;
