33b4d5af87eab1d7a23cca3464f9db2f479ca6bd
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-25 18:18:45 -0400

    add date npm back in

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -33,25 +33,27 @@ const App = () => {
   const { userAuth } = useSelector(selectAuth);
 
   return (
-    <div className="font-rubik">
-      <Navbar userAuth={userAuth} />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/*" element={<NotFound />} />
-        <Route path="/map" element={<Map />} />
-        <Route path="/events" element={<AllEvents />} />
-        <Route path="/events/:id" element={<EventDetails />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<LogIn />} />
-        <Route path="/groups" element={<BrowseGroups />} />
-        <Route path="/groups/:groupId/*" element={<SingleGroup />} />
-        <Route path="/account/*" element={<UserAccount />} />
-        <Route path="/chat" element={<Chat />} />
-        <Route path="/profile/:id/*" element={<UserProfile />} />
-        <Route path="/sitters" element={<DiscoverSitters />} />
-      </Routes>
-      <Footer />
-    </div>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <div className="font-rubik">
+        <Navbar userAuth={userAuth} />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/*" element={<NotFound />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="/events" element={<AllEvents />} />
+          <Route path="/events/:id" element={<EventDetails />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/groups" element={<BrowseGroups />} />
+          <Route path="/groups/:groupId/*" element={<SingleGroup />} />
+          <Route path="/account/*" element={<UserAccount />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/profile/:id/*" element={<UserProfile />} />
+          <Route path="/sitters" element={<DiscoverSitters />} />
+        </Routes>
+        <Footer />
+      </div>
+    </LocalizationProvider>
   );
 };
 
