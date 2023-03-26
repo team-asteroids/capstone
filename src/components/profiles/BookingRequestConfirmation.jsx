e7bab31bcee73f7c94adf65aba5a17cd60fd2799
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-26 16:04:13 -0400

    add more bg images

--- src/components/profiles/BookingRequestConfirmation.jsx
+++ src/components/profiles/BookingRequestConfirmation.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const BookingRequestConfirmation = () => {
   return (
-    <div>
-      <h2>Your request was submitted!</h2>
+    <div className="bg-[url('img/bulldog-bg.jpg')] h-[calc(100vh_-_5rem)] bg-center w-screen bg-no-repeat bg-cover">
+      <div>
+        <h2>Your request was submitted!</h2>
+      </div>
     </div>
   );
 };
