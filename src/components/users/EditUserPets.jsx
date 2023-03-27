d271530b2d43c73f978eb20888415d915ae5866c
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-27 12:56:01 -0400

    reuse edit user pets component in user account

--- src/components/users/EditUserPets.jsx
+++ src/components/users/EditUserPets.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectAuth } from '../../slices/authSlice';
+import { UserPetProfiles } from '../index';
 
 function EditUserPets() {
+  const { userAuth } = useSelector(selectAuth);
   return (
     <div className="font-rubik flex flex-col gap-5">
       <div>
         <h2 className="font-rubikmono">Update Pet Profiles</h2>
       </div>
       <div className="h-[calc(100vh_-_20rem)] overflow-auto flex flex-col gap-5">
-        <div>Edit existing pet profile</div>
+        <div>
+          <UserPetProfiles user={userAuth} />
+        </div>
         <div>Add new pet profile</div>
       </div>
     </div>
