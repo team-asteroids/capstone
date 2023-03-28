d3e359634db8615d590ce7bcebc168917b3a7db2
Nica Weisinger <nica.weisinger@gmail.com>
2023-03-28 09:01:22 -0400

    Pagination component

--- src/components/howls/AllHowls.jsx
+++ src/components/howls/AllHowls.jsx
@@ -1,19 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchAllPosts, fetchAllPostLikes } from '../../slices/postsSlice';
+import Pagination from '../ui/Pagination';
 import Howl from './Howl';
 
 const AllHowls = () => {
   const dispatch = useDispatch();
   const posts = useSelector((state) => state.posts.allPosts);
-  const likes = useSelector((state) => state.posts.postLikes);
+  // const likes = useSelector((state) => state.posts.postLikes);
   //   console.log('posts --> ', posts[0]);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(10);
+
+  // Get current posts
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = posts.slice(indexOfFirstItem, indexOfLastItem);
+  const nPages = Math.ceil(posts.length / itemsPerPage);
+
+  // Change the page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   useEffect(() => {
     const getData = async () => {
       await dispatch(fetchAllPosts());
-      await dispatch(fetchAllPostLikes());
+      // await dispatch(fetchAllPostLikes());
     };
     getData();
   }, [dispatch]);
@@ -24,16 +37,24 @@ const AllHowls = () => {
         <div className="bg-white-smoke border rounded-lg shadow-lg">
           <div className="p-4">
             <div>
-              {posts.map((post) => (
+              {currentItems.map((post) => (
                 <div key={post.id}>
                   <Howl
                     key={post.id}
                     post={post}
-                    likes={likes.filter((like) => like.postId === post.id)}
+                    // likes={likes.filter((like) => like.postId === post.id)}
                   />
                 </div>
               ))}
-            </div>
+            </div>{' '}
+            <br></br>
+            <Pagination
+              nPages={nPages}
+              totalItems={posts.length}
+              paginate={paginate}
+              currentPage={currentPage}
+              setCurrentPage={setCurrentPage}
+            />
           </div>
         </div>
         {/* <div>
---
+++ src/components/ui/Pagination.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+
+const Pagination = ({ nPages, currentPage, setCurrentPage }) => {
+  const pageNumbers = [...Array(nPages + 1).keys()].slice(1);
+
+  const nextPage = () => {
+    if (currentPage !== nPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+  const prevPage = () => {
+    if (currentPage !== 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  return (
+    <nav>
+      <ul className="inline-flex items-center -space-x-px">
+        <li>
+          <button
+            onClick={prevPage}
+            className="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          >
+            <span className="sr-only">Previous</span>
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </button>
+        </li>
+        {pageNumbers.map((number) => (
+          <li
+            className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            key={number}
+          >
+            <button onClick={() => setCurrentPage(number)}>{number}</button>
+          </li>
+        ))}
+        <li>
+          <button
+            onClick={nextPage}
+            className="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          >
+            <span className="sr-only">Next</span>
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
