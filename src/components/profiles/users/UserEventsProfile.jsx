d9dd9012ec18b4290e5d1a8827753341d637bc49
Forhad Zinnah <forhad@Forhads-Mac-mini.local>
2023-03-30 12:50:04 -0700

    displaying upcoming events on user profile

--- src/components/profiles/users/UserEventsProfile.jsx
+++ src/components/profiles/users/UserEventsProfile.jsx
@@ -1,27 +1,48 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
-import { getMyRsvpsAsync, selectMyRsvps } from '../../../slices/eventsSlice';
+import {
+  getMyRsvpsAsync,
+  selectMyRsvps,
+  fetchAllEvents,
+  selectEvents,
+} from '../../../slices/eventsSlice';
+import { format } from 'date-fns';
 
 const UserEventsProfile = (props) => {
   const { user } = props;
   const { id } = useParams();
   const rsvps = useSelector(selectMyRsvps);
+  const { events } = useSelector(selectEvents);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getMyRsvpsAsync(id));
+    dispatch(fetchAllEvents());
   }, [dispatch, id]);
 
-  console.log(rsvps);
   return (
     <div>
       <h2 className="font-semibold text-sm">UPCOMING EVENTS</h2>
       <ul>
         {rsvps.map((rsvp) => (
-          <Link to={`/events/${rsvp.eventId}`}>
-            <li>Event Id: {rsvp.eventId}</li>
-          </Link>
+          <>
+            <Link to={`/events/${rsvp.eventId}`}>
+              {events.map((event) =>
+                event.id === rsvp.eventId ? (
+                  <div className="border-solid border-2 rounded-lg bg-slate">
+                    <li>Event Id: {rsvp.eventId}</li>
+                    <li>Type: {event.topic}</li>
+                    <li>Description: {event.description}</li>
+                    <li>Location: {event.zip_code}</li>
+                  </div>
+                ) : (
+                  <li></li>
+                )
+              )}
+            </Link>
+            <></>
+          </>
         ))}
       </ul>
     </div>
