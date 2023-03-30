3d44b3518bffed9542789857f6df31c1d505667f
Forhad Zinnah <forhad@Forhads-Mac-mini.local>
2023-03-30 11:11:08 -0700

    upcoming events in userProfile now showing eventId's for rsvps

--- src/components/profiles/users/UserEventsProfile.jsx
+++ src/components/profiles/users/UserEventsProfile.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { getMyRsvpsAsync, selectMyRsvps } from '../../../slices/eventsSlice';
 
 const UserEventsProfile = (props) => {
   const { user } = props;
+  const { id } = useParams();
+  const rsvps = useSelector(selectMyRsvps);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getMyRsvpsAsync(id));
+  }, [dispatch, id]);
+
+  console.log(rsvps);
   return (
     <div>
       <h2 className="font-semibold text-sm">UPCOMING EVENTS</h2>
+      <ul>
+        {rsvps.map((rsvp) => (
+          <li>{rsvp.eventId}</li>
+        ))}
+      </ul>
     </div>
   );
 };
