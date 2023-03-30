a0eb8606c879d9bd792f1b879e6fb3a5773c9591
Nica Weisinger <nica.weisinger@gmail.com>
2023-03-30 15:55:16 -0400

    Filter applied to groups but needs to render

--- src/components/groups/BrowseGroups.jsx
+++ src/components/groups/BrowseGroups.jsx
@@ -8,7 +8,9 @@ import Pagination from '../ui/Pagination';
 const BrowseGroups = () => {
   const dispatch = useDispatch();
   const groups = useSelector((state) => state.groups.allGroups);
+
   const [search, setSearch] = useState('');
+  const [topic, setTopic] = useState('');
 
   // Pagination setup
   const [currentPage, setCurrentPage] = useState(1);
@@ -32,6 +34,7 @@ const BrowseGroups = () => {
 
   return (
     <>
+      <h3 className=" text-bold-purple text-lg font-rubikmono ">Groups</h3>
       <div className="bg-white-smoke border rounded-lg shadow-lg">
         <form onSubmit={handleSubmit}>
           <input
@@ -48,19 +51,38 @@ const BrowseGroups = () => {
           </button>
         </form>
       </div>
+      <div>
+        <div className="bg-white-smoke border rounded-lg shadow-lg font-rubik">
+          <div className="p-2">
+            <h2 className="font-rubikmono">Filter by Topic</h2>
+            <select value={topic} onChange={(e) => setTopic(e.target.value)}>
+              <option value="playdates">All</option>
+              <option value="playdates">Playdates</option>
+              <option value="recommendations">Recommendations</option>
+              <option value="exotic pets">Exotic Pets</option>
+              <option value="neighborhood">Neighborhood</option>
+            </select>
+          </div>
+        </div>
+      </div>
       <div className="p-6 bg-[#fca5a5]">
-        <h3 className=" text-bold-purple text-lg font-rubikmono ">Groups</h3>
         <h3 className="text-lg font-rubikmono ">
           <Link to={`/groups/create`}>
             <li>Add New Group</li>
           </Link>
         </h3>
         <div className="p-6 grid grid-cols-3 gap-8 font-mono">
-          {currentItems.map((group) => (
-            <div key={group.group.id} id="cardItem" className="col-xs-2">
-              <Group group={group.group} members={group.members} />
-            </div>
-          ))}
+          {currentItems
+            .filter((group) => {
+              if (group.topic === topic) {
+                return group;
+              }
+            })
+            .map((group) => (
+              <div key={group.group.id} id="cardItem" className="col-xs-2">
+                <Group group={group.group} members={group.members} />
+              </div>
+            ))}
         </div>
         <br></br>
         <Pagination
