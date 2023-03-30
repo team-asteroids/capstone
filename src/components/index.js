dbde7cc18706a62769d423c81d1a9feba05b0ddb
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-30 15:40:49 -0400

    add uer booking details path

--- src/components/index.js
+++ src/components/index.js
@@ -46,3 +46,4 @@ export { default as AddNewPet } from './users/AddNewPet.jsx';
 export { default as BookingCard } from './users/sitters/BookingCard.jsx';
 export { default as SitterClients } from './users/sitters/SitterClients.jsx';
 export { default as EditEvent } from './events/EditEvent.jsx';
+export { default as UserBookingCardDetails } from './users/UserBookingCardDetails.jsx';
