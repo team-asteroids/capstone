a95772fa2e44dfb5fa1d33d1a318f5b9d0aeebc5
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-31 14:16:29 -0400

    submit rating and review functionality

--- src/components/profiles/RatingsAndReviews.jsx
+++ src/components/profiles/RatingsAndReviews.jsx
@@ -7,6 +7,7 @@ import {
   fetchAllBookings,
   resetBookingStatus,
 } from '../../slices/bookingsSlice';
+import { postSitterRating, postSitterReview } from '../../slices/usersSlice';
 import { format } from 'date-fns';
 
 const RatingsAndReviews = (props) => {
@@ -19,6 +20,11 @@ const RatingsAndReviews = (props) => {
   const { allBookings } = useSelector(selectBookings);
 
   const [relevantBookings, setRelevantBookings] = useState([]);
+
+  const [sitterRating, setSitterRating] = useState('');
+  const [sitterReview, setSitterReview] = useState('');
+  const [invalidRating, setInvalidRating] = useState(false);
+  const [invalidReview, setInvalidReview] = useState(false);
 
   useEffect(() => {
     if (userAuth && userAuth.id) {
@@ -41,29 +47,64 @@ const RatingsAndReviews = (props) => {
 
   const labelClass = 'text-xs font-rubikmono';
 
+  const buttonClass =
+    'text-sm font-semibold ease-in-out duration-100 hover:text-bold-pink pb-2';
+
   const validClass =
     'appearance-none block w-full bg-white-200 border rounded py-3 px-6 leading-tight focus:outline-none focus:bg-white focus:border-bold-blue mt-1 font-rubik';
+
+  const invalidClass =
+    'border border-red-500 appearance-none block w-full bg-white-200 border rounded py-3 px-6 leading-tight focus:outline-none focus:bg-white focus:border-bold-blue mt-1 font-rubik';
 
   const goBack = () => {
     navigate(-1);
   };
 
-  const submitRatingAndReview = (evt) => {
+  const submitRatingAndReview = async (evt) => {
     evt.preventDefault();
-    console.log('submitted!');
+    const id = userAuth.id;
+    const sitterId = sitter.id;
+    let ratingRes;
+    let reviewRes;
+
+    if (sitterRating && sitterReview) {
+      ratingRes = await dispatch(
+        postSitterRating({ id, sitterId, token, sitterRating })
+      );
+      console.log(ratingRes);
+      setInvalidRating(false);
+
+      reviewRes = await dispatch(
+        postSitterReview({ id, sitterId, token, sitterReview })
+      );
+      console.log(reviewRes);
+      setInvalidReview(false);
+    } else {
+      setInvalidRating(true);
+      setInvalidReview(true);
+    }
   };
 
   return (
-    <div className="font-rubik flex flex-col gap-5">
+    <div className="font-rubik flex flex-col gap-2">
       <div>
         <button
-          className="text-left max-w-fit text-xs font-semibold hover:text-bold-purple ease-in-out duration-100"
+          className="text-left max-w-fit text-xs pb-3 font-semibold hover:text-bold-purple ease-in-out duration-100"
           onClick={goBack}
         >
           BACK
         </button>
       </div>
       <h2 className="font-rubikmono">Throw A Dog A Bone</h2>
+      <p
+        className={
+          invalidRating || invalidReview
+            ? 'text-red-500 font-rubik font-bold text-sm text-left'
+            : 'collapse mb-1 font-rubik text-xs'
+        }
+      >
+        Please submit a valid rating and review!
+      </p>
       {relevantBookings && relevantBookings.length >= 1 ? (
         <section className="w-2/3">
           <form onSubmit={submitRatingAndReview}>
@@ -82,25 +123,42 @@ const RatingsAndReviews = (props) => {
               <div className="w-1/2 flex flex-col">
                 <label className={labelClass}>Rating</label>
                 <input
-                  className={validClass}
+                  className={invalidRating ? invalidClass : validClass}
                   type="number"
                   step={0.5}
                   min={0}
                   max={5}
+                  value={sitterRating}
+                  onChange={(evt) => {
+                    setInvalidRating(false);
+                    setInvalidReview(false);
+                    setSitterRating(evt.target.value);
+                  }}
                 />
               </div>
             </div>
             <div className="flex flex-wrap mb-5">
               <div className="w-full flex flex-col">
                 <label className={labelClass}>Review</label>
-                <textarea className={validClass} rows={6} />
+                <textarea
+                  className={invalidReview ? invalidClass : validClass}
+                  rows={6}
+                  value={sitterReview}
+                  onChange={(evt) => {
+                    setInvalidReview(false);
+                    setInvalidRating(false);
+                    setSitterReview(evt.target.value);
+                  }}
+                />
               </div>
             </div>
-            <button type="submit">Submit</button>
+            <button className={buttonClass} type="submit">
+              SUBMIT
+            </button>
           </form>
         </section>
       ) : (
-        'must have a booking with sitter to leave a review!'
+        'must have a booked this sitter to leave a review!'
       )}
     </div>
   );
--- src/slices/usersSlice.js
+++ src/slices/usersSlice.js
@@ -25,11 +25,59 @@ export const fetchSingleUser = createAsyncThunk(
   }
 );
 
+// post new sitter rating
+export const postSitterRating = createAsyncThunk(
+  'submitRating',
+  async ({ id, sitterId, token, sitterRating }, { rejectWithValue }) => {
+    try {
+      if (!sitterRating) return rejectWithValue('no rating submitted');
+      const { data } = await axios.post(
+        `/api/users/${id}/ratings`,
+        { sitterId, rating: sitterRating },
+        {
+          headers: {
+            authorization: token,
+          },
+        }
+      );
+      return data;
+    } catch (err) {
+      return rejectWithValue(err);
+    }
+  }
+);
+
+// post new sitter review
+export const postSitterReview = createAsyncThunk(
+  'submitReview',
+  async ({ id, sitterId, token, sitterReview }, { rejectWithValue }) => {
+    try {
+      if (!sitterReview) return rejectWithValue('no rating submitted');
+      const { data } = await axios.post(
+        `/api/users/${id}/reviews`,
+        { sitterId, context: sitterReview },
+        {
+          headers: {
+            authorization: token,
+          },
+        }
+      );
+      return data;
+    } catch (err) {
+      return rejectWithValue(err);
+    }
+  }
+);
+
 const userSlice = createSlice({
   name: 'users',
   initialState: {
     allUsers: [],
     singleUser: {},
+    allReviews: [],
+    singleReview: {},
+    allRatings: [],
+    singleRating: {},
     status: '',
     error: '',
   },
@@ -64,6 +112,32 @@ const userSlice = createSlice({
         state.error = '';
       })
       .addCase(fetchSingleUser.rejected, (state, { payload }) => {
+        state.status = 'failed';
+        state.error = payload.message;
+      })
+      .addCase(postSitterRating.fulfilled, (state, { payload }) => {
+        state.singleRating = payload || {};
+        state.status = 'success';
+        state.error = '';
+      })
+      .addCase(postSitterRating.pending, (state, { payload }) => {
+        state.status = 'loading';
+        state.error = '';
+      })
+      .addCase(postSitterRating.rejected, (state, { payload }) => {
+        state.status = 'failed';
+        state.error = payload.message;
+      })
+      .addCase(postSitterReview.fulfilled, (state, { payload }) => {
+        state.singleReview = payload || {};
+        state.status = 'success';
+        state.error = '';
+      })
+      .addCase(postSitterReview.pending, (state, { payload }) => {
+        state.status = 'loading';
+        state.error = '';
+      })
+      .addCase(postSitterReview.rejected, (state, { payload }) => {
         state.status = 'failed';
         state.error = payload.message;
       });
