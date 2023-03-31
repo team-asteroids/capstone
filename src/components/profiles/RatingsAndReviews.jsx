cae4dfcb8775cc2ec9e0e1fbd2336447a3b64b7b
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-31 13:26:21 -0400

    add relevant bookings to page

--- src/components/profiles/RatingsAndReviews.jsx
+++ src/components/profiles/RatingsAndReviews.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectAuth, fetchAllUserAuthSitters } from '../../slices/authSlice';
+import {
+  selectBookings,
+  fetchAllBookings,
+  resetBookingStatus,
+} from '../../slices/bookingsSlice';
+import { format } from 'date-fns';
 
 const RatingsAndReviews = (props) => {
   const { user, sitter } = props;
+  const dispatch = useDispatch();
   const navigate = useNavigate();
-  // console.log({ user: user, sitter: sitter });
+
+  const { userAuth, token } = useSelector(selectAuth);
+
+  const { allBookings } = useSelector(selectBookings);
+
+  const [relevantBookings, setRelevantBookings] = useState([]);
+
+  useEffect(() => {
+    if (userAuth && userAuth.id) {
+      const id = userAuth.id;
+      dispatch(fetchAllBookings({ id, token }));
+    }
+  }, [userAuth]);
+
+  useEffect(() => {
+    if (allBookings && allBookings.length) {
+      const filteredBookings = allBookings.filter(
+        (booking) => booking.sitterId === sitter.id
+      );
+      setRelevantBookings(filteredBookings);
+    }
+    return () => {
+      dispatch(resetBookingStatus());
+    };
+  }, [allBookings]);
 
   const labelClass = 'text-xs font-rubikmono';
 
@@ -31,36 +64,44 @@ const RatingsAndReviews = (props) => {
         </button>
       </div>
       <h2 className="font-rubikmono">Throw A Dog A Bone</h2>
-      <section className="w-2/3">
-        <form onSubmit={submitRatingAndReview}>
-          <div className="flex flex-wrap mb-5">
-            <div className="w-1/2 flex flex-col pr-6">
-              <label className={labelClass}>Select Sitting</label>
-              <select className={validClass}>
-                <option>sitting1</option>
-                <option>sitting2</option>
-              </select>
+      {relevantBookings && relevantBookings.length >= 1 ? (
+        <section className="w-2/3">
+          <form onSubmit={submitRatingAndReview}>
+            <div className="flex flex-wrap mb-5">
+              <div className="w-1/2 flex flex-col pr-6">
+                <label className={labelClass}>Select Sitting</label>
+                <select className={validClass}>
+                  {relevantBookings.map((booking) => (
+                    <option key={booking.id}>{`Id: ${booking.id} - ${format(
+                      new Date(booking.startDate),
+                      'MMM yyyy'
+                    )}`}</option>
+                  ))}
+                </select>
+              </div>
+              <div className="w-1/2 flex flex-col">
+                <label className={labelClass}>Rating</label>
+                <input
+                  className={validClass}
+                  type="number"
+                  step={0.5}
+                  min={0}
+                  max={5}
+                />
+              </div>
             </div>
-            <div className="w-1/2 flex flex-col">
-              <label className={labelClass}>Rate</label>
-              <input
-                className={validClass}
-                type="number"
-                step={0.5}
-                min={0}
-                max={5}
-              />
+            <div className="flex flex-wrap mb-5">
+              <div className="w-full flex flex-col">
+                <label className={labelClass}>Review</label>
+                <textarea className={validClass} rows={6} />
+              </div>
             </div>
-          </div>
-          <div className="flex flex-wrap mb-5">
-            <div className="w-full flex flex-col">
-              <label className={labelClass}>Review</label>
-              <textarea className={validClass} rows={6} />
-            </div>
-          </div>
-          <button type="submit">Submit</button>
-        </form>
-      </section>
+            <button type="submit">Submit</button>
+          </form>
+        </section>
+      ) : (
+        'must have a booking with sitter to leave a review!'
+      )}
     </div>
   );
 };
