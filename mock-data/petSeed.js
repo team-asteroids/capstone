c85bc56cefc169b3d06cdb60086750327b527cb3
Nica Weisinger <nica.weisinger@gmail.com>
2023-04-01 19:04:32 -0400

    Pet photos added and Pet seed edited

--- mock-data/petSeed.js
+++ mock-data/petSeed.js
@@ -1,38 +1,42 @@
 const pets = [
   {
     name: 'Frank',
-    breed: 'American Eskimo Dogs',
+    breed: 'Golden Retrievers',
     age: 13,
     size: 'extra-large',
     sex: 'female',
-    imageSrc: '',
+    imageSrc:
+      'https://cdn.akc.org/content/article-body-image/golden_puppy_dog_pictures.jpg',
     userId: 1,
   },
   {
     name: 'Rufus',
-    breed: 'Silky Terriers',
+    breed: 'Labrador Retrievers',
     age: 3,
     size: 'large',
     sex: 'male',
-    imageSrc: '',
+    imageSrc:
+      'https://www.thedogandfriends.com/assets/img/index/img-hero_dog.png',
     userId: 2,
   },
   {
     name: 'Buddy',
-    breed: 'Bearded Collies',
+    breed: 'Boxers',
     age: 5,
     size: 'large',
     sex: 'male',
-    imageSrc: '',
+    imageSrc:
+      'https://a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2019/09/1200/675/iStock-500726617.jpg?ve=1&tl=1',
     userId: 1,
   },
   {
     name: 'Lola',
-    breed: 'Silky Terriers',
+    breed: 'Pugs',
     age: 7,
     size: 'small',
     sex: 'female',
-    imageSrc: '',
+    imageSrc:
+      'https://www.thesprucepets.com/thmb/hxWjs7evF2hP1Fb1c1HAvRi_Rw0=/2765x0/filters:no_upscale():strip_icc()/chinese-dog-breeds-4797219-hero-2a1e9c5ed2c54d00aef75b05c5db399c.jpg',
     userId: 2,
   },
   {
@@ -41,26 +45,28 @@ const pets = [
     age: 4,
     size: 'medium',
     sex: 'male',
-    imageSrc: '',
+    imageSrc:
+      'https://www.akc.org/wp-content/uploads/2017/11/Belgian-Sheepdog-standing-in-a-field.jpg',
     userId: 3,
   },
   {
     name: 'Bailey',
-    breed: 'Parson Russell Terriers',
-
+    breed: 'Entlebucher Mountain Dogs',
     age: 2,
     size: 'large',
     sex: 'female',
-    imageSrc: '',
+    imageSrc:
+      'https://assets.technologynetworks.com/production/dynamic/images/content/361072/dog-breed-is-not-an-accurate-way-to-predict-behavior-361072-960x540.jpg?cb=11742263',
     userId: 4,
   },
   {
     name: 'Charlie',
-    breed: 'Parson Russell Terriers',
+    breed: 'Chihuahuas',
     age: 6,
-    size: 'extra-large',
+    size: 'small',
     sex: 'male',
-    imageSrc: '',
+    imageSrc:
+      'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/59486286/1/?bust=1673903094&width=667',
     userId: 5,
   },
   {
@@ -69,34 +75,38 @@ const pets = [
     age: 1,
     size: 'small',
     sex: 'female',
-    imageSrc: '',
+    imageSrc:
+      'https://www.cdc.gov/healthypets/images/pets/cute-dog-headshot.jpg?_=42445',
     userId: 6,
   },
   {
     name: 'Rocky',
-    breed: 'Shih Tzu',
-    age: 9,
+    breed: 'Siberian Huskies',
+    age: 2,
     size: 'large',
     sex: 'male',
-    imageSrc: '',
+    imageSrc:
+      'https://kc-media-cdn-live.azureedge.net/cache/c/a/b/3/a/2/cab3a23ae84ce11e4e4659fb3f884ebeee690b9f.jpg',
     userId: 7,
   },
   {
     name: 'Maggie',
-    breed: 'Bernese Mountain Dogs',
+    breed: 'Pembroke Welsh Corgis',
     age: 8,
     size: 'medium',
     sex: 'female',
-    imageSrc: '',
+    imageSrc:
+      'https://hips.hearstapps.com/goodhousekeeping/assets/17/30/pembroke-welsh-corgi.jpg',
     userId: 8,
   },
   {
     name: 'Toby',
-    breed: 'Salukis',
+    breed: 'Australian Shepherds',
     age: 5,
     size: 'small',
     sex: 'male',
-    imageSrc: '',
+    imageSrc:
+      'https://assets1.cbsnewsstatic.com/hub/i/r/2020/05/05/7f40e69e-da4e-4f48-891f-ff44c7483f15/thumbnail/640x427/df92cca498226e6548fe9ec617955926/gettyimages-506082982.jpg',
     userId: 9,
   },
   {
@@ -105,34 +115,37 @@ const pets = [
     age: 4,
     size: 'medium',
     sex: 'female',
-    imageSrc: '',
+    imageSrc:
+      'https://www.cesarsway.com/wp-content/uploads/2015/06/7-reasons-to-adopt-a-senior-dog.jpg',
     userId: 10,
   },
   {
     name: 'Jack',
-    breed: 'Ibizan Hounds',
+    breed: 'Mixed',
     age: 2,
     size: 'small',
     sex: 'male',
-    imageSrc: '',
+    imageSrc:
+      'https://hips.hearstapps.com/hmg-prod/images/smartest-dog-breeds-1651788779.jpg?crop=1xw:0.84375xh;center,top&resize=1200:*',
     userId: 11,
   },
   {
     name: 'Bella',
-    breed: 'Chinooks',
+    breed: 'Cavalier King Charles Spaniels',
     age: 1,
     size: 'small',
     sex: 'female',
-    imageSrc: '',
+    imageSrc:
+      'https://www.akc.org/wp-content/uploads/2021/07/Cavalier-King-Charles-Spaniel-laying-down-indoors.jpeg',
     userId: 12,
   },
   {
     name: 'Bear',
-    breed: 'Canaan Dogs',
+    breed: 'Labrador Retrievers',
     age: 6,
     size: 'extra-large',
     sex: 'male',
-    imageSrc: '',
+    imageSrc: 'https://images.indianexpress.com/2022/05/dogs_1200.jpg',
     userId: 13,
   },
   {
@@ -141,16 +154,18 @@ const pets = [
     age: 3,
     size: 'medium',
     sex: 'female',
-    imageSrc: '',
+    imageSrc:
+      'https://www.southernliving.com/thmb/WHH7cdFT3YMJlJN4y7y3lsAKvJ8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/gettyimages-114166947-1-268128f97e5c415baede328c1fe32f55.jpg',
     userId: 14,
   },
   {
     name: 'Cooper',
     breed: 'Belgian Laekenois',
     age: 5,
-    size: 'extra-large',
+    size: 'large',
     sex: 'male',
-    imageSrc: '',
+    imageSrc:
+      'https://a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2019/12/1200/675/yellow-labrador-dog-iStock.jpg?ve=1&tl=1',
     userId: 15,
   },
 ];
