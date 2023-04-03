6463a09bc764d2c1a387e0a99126be90792647b0
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-04-02 21:29:44 -0400

    update create event form

--- src/components/events/CreateEvent.jsx
+++ src/components/events/CreateEvent.jsx
@@ -83,7 +83,7 @@ const CreateEvent = () => {
     'appearance-none rounded-lg block w-full bg-white-200 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-bold-blue mt-3 font-rubik';
 
   return (
-    <div className="p-6 bg-pale-blue h-[calc(100vh_-_5rem)]">
+    <div className="bg-cover bg-no-repeat bg-center bg-[url('img/events-bg.jpg')] h-[calc(100vh_-_5rem)]">
       {created ? (
         <div className="">
           <p>Event successfully created!</p>
@@ -94,199 +94,194 @@ const CreateEvent = () => {
           </Link>
         </div>
       ) : (
-        <div className="w-100 h-vh100 bg-pale-blue">
-          <p
-            className={
-              failMessage
-                ? 'text-red-500 font-rubik text-center font-bold text-sm mt-6'
-                : 'collapse font-rubik text-xs'
-            }
-          >
-            Cannot create event.
-          </p>
-          <h3 className=" text-bold-blue text-lg font-rubikmono ">
-            Create New Event
-          </h3>
-          <div className="flex flex-wrap">
-            <form onSubmit={submitEvent}>
-              <div className="grid gap-6 mb-6 md:grid-cols-2">
-                <div>
-                  <label htmlFor="event_start" className={labelClass}>
-                    Event Start:
-                  </label>
-                  <input
-                    type="datetime-local"
-                    id="event_start"
-                    value={eventStart}
-                    onChange={(e) => {
-                      setIsInvalidEventStart(false);
-                      setEventStartPrompt(false);
-                      setEventStart(e.target.value);
-                    }}
-                    className={validClass}
-                    // required
-                  ></input>
-                  <p
-                    className={
-                      eventStartPrompt
-                        ? 'text-xs mt-2 text-red-500'
-                        : 'collapse -mt-2'
-                    }
-                  >
-                    Please enter a start date/time
-                  </p>
-                </div>
-                <div>
-                  <label
-                    htmlFor="event_end"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Event End:
-                  </label>
-                  <input
-                    type="datetime-local"
-                    id="event_end"
-                    value={eventEnd}
-                    onChange={(e) => {
-                      setIsInvalidEventEnd(false);
-                      setEventEndPrompt(false);
-                      setEventEnd(e.target.value);
-                    }}
-                    className={validClass}
-                    // required
-                  ></input>
-                  <p
-                    className={
-                      eventEndPrompt
-                        ? 'text-xs mt-2 text-red-500'
-                        : 'collapse -mt-2'
-                    }
-                  >
-                    Please enter a end date/time
-                  </p>
+        <div className="flex flex-row">
+          <div className="w-1/2"></div>
+          <div className="w-1/2 pt-20 px-20 justify-center">
+            <p
+              className={
+                failMessage
+                  ? 'text-red-500 font-rubik text-center font-bold text-sm mt-6'
+                  : 'collapse font-rubik text-xs'
+              }
+            >
+              Cannot create event.
+            </p>
+            <h3 className="text-4xl font-rubikmono text-center pb-10">
+              Create New Event
+            </h3>
+            <div className="flex flex-wrap">
+              <form onSubmit={submitEvent}>
+                <div className="flex flex-wrap mb-5">
+                  <div className="w-1/2 flex flex-col pr-6">
+                    <label htmlFor="event_start" className={labelClass}>
+                      Start
+                    </label>
+                    <input
+                      type="datetime-local"
+                      id="event_start"
+                      value={eventStart}
+                      onChange={(e) => {
+                        setIsInvalidEventStart(false);
+                        setEventStartPrompt(false);
+                        setEventStart(e.target.value);
+                      }}
+                      className={validClass}
+                      // required
+                    ></input>
+                    <p
+                      className={
+                        eventStartPrompt
+                          ? 'text-xs mt-2 text-red-500'
+                          : 'collapse -mt-2'
+                      }
+                    >
+                      Please enter a start date/time
+                    </p>
+                  </div>
+
+                  <div className="w-1/2 flex flex-col">
+                    <label htmlFor="event_end" className={labelClass}>
+                      End
+                    </label>
+                    <input
+                      type="datetime-local"
+                      id="event_end"
+                      value={eventEnd}
+                      onChange={(e) => {
+                        setIsInvalidEventEnd(false);
+                        setEventEndPrompt(false);
+                        setEventEnd(e.target.value);
+                      }}
+                      className={validClass}
+                      // required
+                    ></input>
+                    <p
+                      className={
+                        eventEndPrompt
+                          ? 'text-xs mt-2 text-red-500'
+                          : 'collapse -mt-2'
+                      }
+                    >
+                      Please enter a end date/time
+                    </p>
+                  </div>
+
+                  <div className="w-1/2 flex flex-col pr-6">
+                    <label htmlFor="topic" className={labelClass}>
+                      Category
+                    </label>
+                    <input
+                      type="text"
+                      id="topic"
+                      value={topic}
+                      onChange={(e) => {
+                        setIsInvalidTopic(false);
+                        setTopicPrompt(false);
+                        setTopic(e.target.value);
+                      }}
+                      className={validClass}
+                      placeholder="Birthday, Group Playdate, etc..."
+                      // required
+                    ></input>
+                    <p
+                      className={
+                        topicPrompt
+                          ? 'text-xs mt-2 text-red-500'
+                          : 'collapse -mt-2'
+                      }
+                    >
+                      This field must be filled
+                    </p>
+                  </div>
+                  <div className="w-1/2 flex flex-col">
+                    <label htmlFor="zip_code" className={labelClass}>
+                      Zip Code
+                    </label>
+                    <input
+                      type="number"
+                      id="zip_code"
+                      value={zipCode}
+                      onChange={(e) => {
+                        setIsInvalidZipCode(false);
+                        setZipPrompt(false);
+                        setZipCode(e.target.value);
+                      }}
+                      className={validClass}
+                      placeholder="01010"
+                      // required
+                    ></input>
+                    <p
+                      className={
+                        zipPrompt
+                          ? 'text-xs mt-2 text-red-500'
+                          : 'collapse -mt-2'
+                      }
+                    >
+                      Please enter a valid zip code!
+                    </p>
+                  </div>
+
+                  <div className="w-full">
+                    <label htmlFor="description" className={labelClass}>
+                      Description
+                    </label>
+                    <input
+                      type="text"
+                      id="description"
+                      value={description}
+                      onChange={(e) => {
+                        setIsInvalidDescription(false);
+                        setDescriptionPrompt(false);
+                        setDescription(e.target.value);
+                      }}
+                      className={validClass}
+                      placeholder="Tell us a little bit more about this event"
+                      // required
+                    ></input>
+                    <p
+                      className={
+                        descriptionPrompt
+                          ? 'text-xs mt-2 text-red-500'
+                          : 'collapse -mt-2'
+                      }
+                    >
+                      Please enter a description of this event!
+                    </p>
+                  </div>
                 </div>
 
-                <div>
+                <div className="flex items-start mb-6">
+                  <div className="flex items-center h-5">
+                    <input
+                      id="remember"
+                      type="checkbox"
+                      value=""
+                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
+                      // required
+                    ></input>
+                  </div>
                   <label
-                    htmlFor="topic"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    htmlFor="remember"
+                    className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
                   >
-                    Type of Event:
+                    I agree with the{' '}
+                    <a
+                      href="/"
+                      className="text-blue-600 hover:underline dark:text-blue-500"
+                    >
+                      terms and conditions
+                    </a>
+                    .
                   </label>
-                  <input
-                    type="text"
-                    id="topic"
-                    value={topic}
-                    onChange={(e) => {
-                      setIsInvalidTopic(false);
-                      setTopicPrompt(false);
-                      setTopic(e.target.value);
-                    }}
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Birthday, Group Playdate, etc..."
-                    // required
-                  ></input>
-                  <p
-                    className={
-                      topicPrompt
-                        ? 'text-xs mt-2 text-red-500'
-                        : 'collapse -mt-2'
-                    }
-                  >
-                    This feild must be filled
-                  </p>
                 </div>
-                <div>
-                  <label
-                    htmlFor="zip_code"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Location (Zip Code) :
-                  </label>
-                  <input
-                    type="number"
-                    id="zip_code"
-                    value={zipCode}
-                    onChange={(e) => {
-                      setIsInvalidZipCode(false);
-                      setZipPrompt(false);
-                      setZipCode(e.target.value);
-                    }}
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="01010"
-                    // required
-                  ></input>
-                  <p
-                    className={
-                      zipPrompt ? 'text-xs mt-2 text-red-500' : 'collapse -mt-2'
-                    }
-                  >
-                    Please enter a valid zip code!
-                  </p>
-                </div>
-              </div>
-              <div className="mb-6">
-                <label
-                  htmlFor="description"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                <button
+                  type="submit"
+                  className="ease-in font-rubikmono duration-300 hover:bg-bold-purple w-full bg-bold-blue text-white py-3 rounded-xl mx-auto block text-xl hover:transition-all mt-8 mb-5"
                 >
-                  Event Details:
-                </label>
-                <input
-                  type="text"
-                  id="description"
-                  value={description}
-                  onChange={(e) => {
-                    setIsInvalidDescription(false);
-                    setDescriptionPrompt(false);
-                    setDescription(e.target.value);
-                  }}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="Tell us a little bit more about this event"
-                  // required
-                ></input>
-                <p
-                  className={
-                    descriptionPrompt
-                      ? 'text-xs mt-2 text-red-500'
-                      : 'collapse -mt-2'
-                  }
-                >
-                  Please enter a description of this event!
-                </p>
-              </div>
-
-              <div className="flex items-start mb-6">
-                <div className="flex items-center h-5">
-                  <input
-                    id="remember"
-                    type="checkbox"
-                    value=""
-                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
-                    // required
-                  ></input>
-                </div>
-                <label
-                  htmlFor="remember"
-                  className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >
-                  I agree with the{' '}
-                  <a
-                    href="/"
-                    className="text-blue-600 hover:underline dark:text-blue-500"
-                  >
-                    terms and conditions
-                  </a>
-                  .
-                </label>
-              </div>
-              <button
-                type="submit"
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Submit
-              </button>
-            </form>
+                  SUBMIT
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       )}
