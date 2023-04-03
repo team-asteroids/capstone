7ae8976c1eaff040a830e74a399171ce80e23bf7
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-04-03 10:25:25 -0400

    remove git push in code

--- src/components/howls/AddHowl.jsx
+++ src/components/howls/AddHowl.jsx
@@ -39,7 +39,7 @@ const AddHowl = () => {
     'w-full appearance-none border-slate-400 leading-tight block rounded-lg p-5 bg-bright-white focus:border-bold-blue focus:outline-none';
 
   const disabledBox =
-    'w-full appearance-nonegit push leading-tight block rounded-lg p-5  border-slate-400 w-full bg-slate-200/50 focus:border-bold-blue focus:outline-none';
+    'w-full appearance-none leading-tight block rounded-lg p-5  border-slate-400 w-full bg-slate-200/50 focus:border-bold-blue focus:outline-none';
 
   const buttonClass =
     'text-sm px-4 py-3 text-bright-white rounded-lg bg-bold-purple font-semibold ease-in-out duration-100 hover:bg-pale-purple';
