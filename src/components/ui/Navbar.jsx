0da1227c22112276e62c85f10906acee0427a9e0
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-04-03 12:11:33 -0400

    fix navbar spacing and image

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -15,45 +15,56 @@ function Navbar(props) {
           <h1 className="text-bold-blue text-5xl">Howlr</h1>
         </Link>
       </div>
+      <div className="flex flex-row">
+        <div className="flex flex-row items-center text-2xl gap-10">
+          <div>
+            <Dropdown label="Social" inline={true}>
+              <Link to={`/events`}>
+                <Dropdown.Item icon={EventIcon}>Events</Dropdown.Item>
+              </Link>
+              <Link to={`/groups`}>
+                <Dropdown.Item icon={GroupsIcon}>Groups</Dropdown.Item>
+              </Link>
+              <Link to={`/howls`}>
+                <Dropdown.Item icon={PetsIcon}>Howls</Dropdown.Item>
+              </Link>
+            </Dropdown>
+          </div>
+          <div>
+            <Link to={`/sitters`}>Sitters</Link>
+          </div>
 
-      <ul className="flex text-2xl gap-10">
-        <Dropdown label="Social" inline={true}>
-          <Link to={`/events`}>
-            <Dropdown.Item icon={EventIcon}>Events</Dropdown.Item>
-          </Link>
-          <Link to={`/groups`}>
-            <Dropdown.Item icon={GroupsIcon}>Groups</Dropdown.Item>
-          </Link>
-          <Link to={`/howls`}>
-            <Dropdown.Item icon={PetsIcon}>Howls</Dropdown.Item>
-          </Link>
-        </Dropdown>
-        <li>
-          <Link to={`/sitters`}>Sitters</Link>
-        </li>
-        {userAuth && userAuth.firstName ? (
-          <>
-            <Link to={'/chat'}>
-              <li>Chat</li>
-            </Link>
-            <Link to={'/account/editprofile'}>
-              <div className="flex flex-row gap-5 justify-center items-center">
-                <div></div>
-                <Avatar
-                  className="w-2 h-2 align-middle pr-5"
-                  img={userAuth.imgSrc}
-                  rounded={true}
-                />
-                <li>Hi, {userAuth.firstName}</li>
-              </div>
-            </Link>
-          </>
-        ) : (
-          <Link to={'/login'}>
-            <li>Login</li>
-          </Link>
-        )}
-      </ul>
+          {userAuth && userAuth.firstName ? (
+            <div>
+              <Link to={'/chat'}>
+                <div>Chat</div>
+              </Link>
+            </div>
+          ) : null}
+          {userAuth && userAuth.firstName ? (
+            <div>
+              <Link to={'/account/editprofile'}>
+                <div className="flex flex-row gap-5 justify-center align-middle items-center">
+                  <img
+                    className="w-12 h-12 object-cover rounded-full"
+                    src={
+                      userAuth.imageSrc || require('../../img/default-dog.jpg')
+                    }
+                    alt="alt"
+                  />
+                  <div>Hi, {userAuth.firstName}</div>
+                </div>
+              </Link>
+            </div>
+          ) : (
+            <div>
+              <Link to={'/login'}>
+                <li>Login</li>
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
     </nav>
   );
 }
