5c2fcb606ed4b8c8c4deaf06414b3e109b4cea98
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-04-05 13:16:18 -0400

    add edit/delete review and rating thunks

--- src/slices/usersSlice.js
+++ src/slices/usersSlice.js
@@ -2,6 +2,7 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const API = process.env.REACT_APP_API_URL;
+// const API = '';
 
 export const fetchAllUsers = createAsyncThunk(
   'allUsers',
@@ -20,6 +21,44 @@ export const fetchSingleUser = createAsyncThunk(
   async (id, { rejectWithValue }) => {
     try {
       const { data } = await axios.get(API + `/api/users/${id}`);
+      return data;
+    } catch (err) {
+      return rejectWithValue(err);
+    }
+  }
+);
+
+export const fetchSitterReview = createAsyncThunk(
+  'fetchReview',
+  async ({ id, token, reviewId }, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.get(
+        API + `/api/users/${id}/reviews/${reviewId}`,
+        {
+          headers: {
+            authorization: token,
+          },
+        }
+      );
+      return data;
+    } catch (err) {
+      return rejectWithValue(err);
+    }
+  }
+);
+
+export const fetchSitterRating = createAsyncThunk(
+  'fetchRating',
+  async ({ id, token, ratingId }, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.get(
+        API + `/api/users/${id}/ratings/${ratingId}`,
+        {
+          headers: {
+            authorization: token,
+          },
+        }
+      );
       return data;
     } catch (err) {
       return rejectWithValue(err);
@@ -49,6 +88,47 @@ export const postSitterRating = createAsyncThunk(
   }
 );
 
+// edit sitter rating
+export const editSitterRating = createAsyncThunk(
+  'editRating',
+  async ({ id, token, ratingId, rating }, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.put(
+        API + `/api/users/${id}/ratings/${ratingId}`,
+        { rating },
+        {
+          headers: {
+            authorization: token,
+          },
+        }
+      );
+      return data;
+    } catch (err) {
+      return rejectWithValue(err);
+    }
+  }
+);
+
+// delete sitter rating
+export const deleteSitterRating = createAsyncThunk(
+  'deleteRating',
+  async ({ id, token, ratingId }, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.delete(
+        API + `/api/users/${id}/ratings/${ratingId}`,
+        {
+          headers: {
+            authorization: token,
+          },
+        }
+      );
+      return data;
+    } catch (err) {
+      return rejectWithValue(err);
+    }
+  }
+);
+
 // post new sitter review
 export const postSitterReview = createAsyncThunk(
   'submitReview',
@@ -58,6 +138,47 @@ export const postSitterReview = createAsyncThunk(
       const { data } = await axios.post(
         API + `/api/users/${id}/reviews`,
         { sitterId, context: sitterReview },
+        {
+          headers: {
+            authorization: token,
+          },
+        }
+      );
+      return data;
+    } catch (err) {
+      return rejectWithValue(err);
+    }
+  }
+);
+
+// edit sitter review
+export const editSitterReview = createAsyncThunk(
+  'editReview',
+  async ({ id, token, reviewId, context }, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.put(
+        API + `/api/users/${id}/reviews/${reviewId}`,
+        { context },
+        {
+          headers: {
+            authorization: token,
+          },
+        }
+      );
+      return data;
+    } catch (err) {
+      return rejectWithValue(err);
+    }
+  }
+);
+
+// delete sitter review
+export const deleteSitterReview = createAsyncThunk(
+  'deleteReview',
+  async ({ id, token, reviewId }, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.delete(
+        API + `/api/users/${id}/reviews/${reviewId}`,
         {
           headers: {
             authorization: token,
@@ -140,6 +261,84 @@ const userSlice = createSlice({
         state.error = '';
       })
       .addCase(postSitterReview.rejected, (state, { payload }) => {
+        state.status = 'failed';
+        state.error = payload.message;
+      })
+      .addCase(deleteSitterReview.fulfilled, (state, { payload }) => {
+        state.singleReview = payload || {};
+        state.status = 'success';
+        state.error = '';
+      })
+      .addCase(deleteSitterReview.pending, (state, { payload }) => {
+        state.status = 'loading';
+        state.error = '';
+      })
+      .addCase(deleteSitterReview.rejected, (state, { payload }) => {
+        state.status = 'failed';
+        state.error = payload.message;
+      })
+      .addCase(editSitterReview.fulfilled, (state, { payload }) => {
+        state.singleReview = payload || {};
+        state.status = 'success';
+        state.error = '';
+      })
+      .addCase(editSitterReview.pending, (state, { payload }) => {
+        state.status = 'loading';
+        state.error = '';
+      })
+      .addCase(editSitterReview.rejected, (state, { payload }) => {
+        state.status = 'failed';
+        state.error = payload.message;
+      })
+      .addCase(deleteSitterRating.fulfilled, (state, { payload }) => {
+        state.singleRating = payload || {};
+        state.status = 'success';
+        state.error = '';
+      })
+      .addCase(deleteSitterRating.pending, (state, { payload }) => {
+        state.status = 'loading';
+        state.error = '';
+      })
+      .addCase(deleteSitterRating.rejected, (state, { payload }) => {
+        state.status = 'failed';
+        state.error = payload.message;
+      })
+      .addCase(editSitterRating.fulfilled, (state, { payload }) => {
+        state.singleRating = payload || {};
+        state.status = 'success';
+        state.error = '';
+      })
+      .addCase(editSitterRating.pending, (state, { payload }) => {
+        state.status = 'loading';
+        state.error = '';
+      })
+      .addCase(editSitterRating.rejected, (state, { payload }) => {
+        state.status = 'failed';
+        state.error = payload.message;
+      })
+      .addCase(fetchSitterReview.fulfilled, (state, { payload }) => {
+        state.singleReview = payload || {};
+        state.status = 'success';
+        state.error = '';
+      })
+      .addCase(fetchSitterReview.pending, (state, { payload }) => {
+        state.status = 'loading';
+        state.error = '';
+      })
+      .addCase(fetchSitterReview.rejected, (state, { payload }) => {
+        state.status = 'failed';
+        state.error = payload.message;
+      })
+      .addCase(fetchSitterRating.fulfilled, (state, { payload }) => {
+        state.singleRating = payload || {};
+        state.status = 'success';
+        state.error = '';
+      })
+      .addCase(fetchSitterRating.pending, (state, { payload }) => {
+        state.status = 'loading';
+        state.error = '';
+      })
+      .addCase(fetchSitterRating.rejected, (state, { payload }) => {
         state.status = 'failed';
         state.error = payload.message;
       });
