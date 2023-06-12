b5dc3231219623abd603bb962f9326746a8f38e1
Nica Weisinger <nica.weisinger@gmail.com>
2023-06-12 11:33:32 -0400

    Tweaked homepage responsiveness

--- src/components/ui/Homepage.jsx
+++ src/components/ui/Homepage.jsx
@@ -10,7 +10,7 @@ const Homepage = () => {
   const { userAuth } = useSelector(selectAuth);
 
   return (
-    <div className="font-rubikmono">
+    <div>
       <img
         className="w-full h-screen object-cover object-left"
         src={backgroundImg}
@@ -19,48 +19,38 @@ const Homepage = () => {
       <div className="w-full h-screen absolute top-0 left-0">
         <div className="max-w-[800px] m-auto h-full w-full flex flex-col justify-center items-center">
           <div className="flex flex-row justify-center">
-            <div className="mb-10 ml-4 bg-[#FF5BA2] rounded-lg">
-              <div className="pt-10 px-8 pb-8 flex flex-wrap gap-4 justify-center items-center sm:text-5xl text-4xl text-center">
+            <div className="mb-10 ml-4 bg-bold-pink rounded-lg">
+              <div className="pt-10 px-8 pb-8 flex flex-wrap gap-4 justify-center items-center font-rubikmono sm:text-5xl text-4xl text-center">
                 <p>Welcome to the</p>
                 <p>Howlr Pack!</p>
               </div>
-              <div className="pt-8 px-8 pb-6 justify-center items-center flex flex-wrap text-center">
+              <div className="pt-8 px-8 pb-6 justify-center items-center flex flex-wrap font-rubik text-2xl text-center">
                 <p>Digital dog park for Sitters, Friends, Packmates</p>
               </div>
               {/* </div> */}
             </div>
           </div>
-
-          {/* <div>
-        <p className="leading-none pb-2">Welcome to the</p>
-        <p className="leading-none">Howlr Pack!</p>
-
-        <div>
-          <p>Digital dog park for Sitters, Friends, Packmates</p>
-        </div> */}
-
-          {/* {token ? (
-          <div className="flex flex-col absolute top-[40rem] text-xl left-48">
-            <Link
-              to={`/profile/${userAuth.id}`}
-              className="bg-bold-pink px-6 py-3.5 rounded-xl text-white ease-in duration-200 hover:bg-pale-pink"
-            >
-              My Profile
-            </Link>
-          </div>
-        ) : (
-          <div className="flex flex-col absolute top-[42rem] text-xl left-48 md:pl-20 p-4 py-16">
-            <Link
-              to="/signup"
-              className="bg-bold-pink px-6 py-3.5 rounded-xl text-white ease-in duration-200 hover:bg-pale-pink"
-            >
-              Sign Up
-            </Link>
-          </div>
-        )} */}
+          {token ? (
+            <div className="flex flex-col justify-center items-center text-xl">
+              <Link
+                to={`/profile/${userAuth.id}`}
+                className="bg-bold-pink px-6 py-3.5 rounded-xl text-white ease-in duration-200 hover:bg-pale-pink"
+              >
+                My Profile
+              </Link>
+            </div>
+          ) : (
+            <div className="flex flex-col justify-center items-center text-xl">
+              <Link
+                to="/signup"
+                className="bg-[#3a86ff] px-6 py-3.5 rounded-xl text-white ease-in duration-200 hover:bg-pale-pink"
+              >
+                Sign Up
+              </Link>
+            </div>
+          )}
         </div>
       </div>
-      //{' '}
     </div>
   );
 };
